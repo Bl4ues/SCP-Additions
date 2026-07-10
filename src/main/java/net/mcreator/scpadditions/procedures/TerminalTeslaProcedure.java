2e8793a097a6f33c31816e79d9eb4e8b1c13dd09
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 06:59:19 -0300

    Make Tesla terminal always open instead of powering on

--- src/main/java/net/mcreator/scpadditions/procedures/TerminalTeslaProcedure.java
+++ src/main/java/net/mcreator/scpadditions/procedures/TerminalTeslaProcedure.java
@@ -1,49 +1,38 @@
 package net.mcreator.scpadditions.procedures;
 
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.network.NetworkHooks;
 
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.scpadditions.init.ScpAdditionsModItems;
-import net.mcreator.scpadditions.init.ScpAdditionsModBlocks;
+import net.mcreator.scpadditions.world.inventory.TeslaTerminalMenu;
 
-import java.util.Map;
+import io.netty.buffer.Unpooled;
 
 public class TerminalTeslaProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-		if (entity == null)
+		if (!(entity instanceof ServerPlayer player)) {
 			return;
-		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.SECURITY_CREDENTIALS.get())) : false) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = ScpAdditionsModBlocks.TESLA_TERMINAL_BLOCK.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-					if (_property != null && _bs.getValue(_property) != null)
-						try {
-							_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:terminalon")), SoundSource.NEUTRAL, 1, 1);
-				} else {
-					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:terminalon")), SoundSource.NEUTRAL, 1, 1, false);
-				}
-			}
 		}
+		BlockPos pos = BlockPos.containing(x, y, z);
+		NetworkHooks.openScreen(player, new MenuProvider() {
+			@Override
+			public Component getDisplayName() {
+				return Component.literal("Tesla Terminal");
+			}
+
+			@Override
+			public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+				return new TeslaTerminalMenu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(pos));
+			}
+		}, pos);
 	}
 }

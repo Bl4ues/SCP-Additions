751c35c470b5c250de9625fcdf923183a4bf1902
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 04:58:29 -0300

    Use shared keycard level logic for level 2 left reader

--- src/main/java/net/mcreator/scpadditions/procedures/Lv2LeftProcedure.java
+++ src/main/java/net/mcreator/scpadditions/procedures/Lv2LeftProcedure.java
@@ -1,120 +1,12 @@
 package net.mcreator.scpadditions.procedures;
 
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
 
-import net.mcreator.scpadditions.init.ScpAdditionsModItems;
 import net.mcreator.scpadditions.init.ScpAdditionsModBlocks;
-
-import java.util.Map;
 
 public class Lv2LeftProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-		if (entity == null)
-			return;
-		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.LEVEL_2_KEYCARD.get())) : false) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-					if (_property != null && _bs.getValue(_property) != null)
-						try {
-							_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-		} else {
-			if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.LEVEL_3_KEYCARD.get())) : false) {
-				{
-					BlockPos _bp = BlockPos.containing(x, y, z);
-					BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT.get().defaultBlockState();
-					BlockState _bso = world.getBlockState(_bp);
-					for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-						Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-						if (_property != null && _bs.getValue(_property) != null)
-							try {
-								_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-							} catch (Exception e) {
-							}
-					}
-					world.setBlock(_bp, _bs, 3);
-				}
-			} else {
-				if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.LEVEL_4_KEYCARD.get())) : false) {
-					{
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT.get().defaultBlockState();
-						BlockState _bso = world.getBlockState(_bp);
-						for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-							Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-							if (_property != null && _bs.getValue(_property) != null)
-								try {
-									_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-								} catch (Exception e) {
-								}
-						}
-						world.setBlock(_bp, _bs, 3);
-					}
-				} else {
-					if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.LEVEL_5_KEYCARD.get())) : false) {
-						{
-							BlockPos _bp = BlockPos.containing(x, y, z);
-							BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT.get().defaultBlockState();
-							BlockState _bso = world.getBlockState(_bp);
-							for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-								Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-								if (_property != null && _bs.getValue(_property) != null)
-									try {
-										_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-									} catch (Exception e) {
-									}
-							}
-							world.setBlock(_bp, _bs, 3);
-						}
-					} else {
-						if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(ScpAdditionsModItems.LEVEL_6_KEYCARD.get())) : false) {
-							{
-								BlockPos _bp = BlockPos.containing(x, y, z);
-								BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-						} else {
-							{
-								BlockPos _bp = BlockPos.containing(x, y, z);
-								BlockState _bs = ScpAdditionsModBlocks.LV_2_LEFT_READER_WRONG.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-						}
-					}
-				}
-			}
-		}
+		KeycardReaderHelper.handleReader(world, x, y, z, entity, 2, ScpAdditionsModBlocks.LV_2_LEFT_READER_ACCEPT, ScpAdditionsModBlocks.LV_2_LEFT_READER_WRONG);
 	}
 }

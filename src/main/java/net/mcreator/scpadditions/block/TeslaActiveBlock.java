ce5f488d1f422e0af6d8f5ea90b14f930022a6b4
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 06:02:59 -0300

    Remove Tesla Gate active collision

--- src/main/java/net/mcreator/scpadditions/block/TeslaActiveBlock.java
+++ src/main/java/net/mcreator/scpadditions/block/TeslaActiveBlock.java
@@ -74,7 +74,7 @@ public class TeslaActiveBlock extends Block implements SimpleWaterloggedBlock {
 
 	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return TeslaGateShapeHelper.shape(state.getValue(FACING));
+		return Shapes.empty();
 	}
 
 	@Override

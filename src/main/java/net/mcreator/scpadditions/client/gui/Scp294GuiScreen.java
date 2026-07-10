4e07dd515099dc6a85aeda08bf81c1dfb6751e5c
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 04:32:43 -0300

    Redesign SCP-294 GUI with custom panel texture

--- src/main/java/net/mcreator/scpadditions/client/gui/Scp294GuiScreen.java
+++ src/main/java/net/mcreator/scpadditions/client/gui/Scp294GuiScreen.java
@@ -1,27 +1,38 @@
 package net.mcreator.scpadditions.client.gui;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringUtil;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.level.Level;
 
-import net.mcreator.scpadditions.world.inventory.Scp294GuiMenu;
-import net.mcreator.scpadditions.network.Scp294GuiButtonMessage;
 import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.network.Scp294GuiButtonMessage;
+import net.mcreator.scpadditions.world.inventory.Scp294GuiMenu;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
 public class Scp294GuiScreen extends AbstractContainerScreen<Scp294GuiMenu> {
+	private static final int TEX_W = 600;
+	private static final int TEX_H = 476;
+	private static final ResourceLocation BACKGROUND = new ResourceLocation("scp_additions:textures/screens/scp_294_gui.png");
+	private static final ResourceLocation COIN_OFF = new ResourceLocation("scp_additions:textures/screens/scp_294_coin_off.png");
+	private static final ResourceLocation COIN_ON = new ResourceLocation("scp_additions:textures/screens/scp_294_coin_on.png");
+	private static final ResourceLocation SCREEN_OVERLAY = new ResourceLocation("scp_additions:textures/screens/scp_294_screen_overlay.png");
+	private static final double[][] INPUT_POLY = {{84, 285}, {399, 286}, {408, 50}, {73, 52}};
+	private static final double[][] COIN_POLY = {{464, 388}, {564, 390}, {579, 227}, {472, 228}};
+	private static final double[][] ENTER_POLY = {{110, 442}, {376, 440}, {364, 330}, {115, 330}};
+
 	private final Level world;
 	private final int x, y, z;
 	private final Player entity;
-	EditBox scp294input;
-	Button button_enter;
+	private String order = "";
+	private boolean inputFocused = true;
+	private double guiScale = 1.0D;
 
 	public Scp294GuiScreen(Scp294GuiMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -30,27 +41,98 @@ public class Scp294GuiScreen extends AbstractContainerScreen<Scp294GuiMenu> {
 		this.y = container.y;
 		this.z = container.z;
 		this.entity = container.entity;
-		this.imageWidth = 176;
-		this.imageHeight = 166;
+		this.imageWidth = TEX_W;
+		this.imageHeight = TEX_H;
 	}
-
-	private static final ResourceLocation texture = new ResourceLocation("scp_additions:textures/screens/scp_294_gui.png");
 
 	@Override
 	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+		updateLayout();
 		this.renderBackground(guiGraphics);
-		super.render(guiGraphics, mouseX, mouseY, partialTicks);
-		scp294input.render(guiGraphics, mouseX, mouseY, partialTicks);
-		this.renderTooltip(guiGraphics, mouseX, mouseY);
+		renderPanel(guiGraphics);
+		renderHoverTooltip(guiGraphics, mouseX, mouseY);
+	}
+
+	private void renderPanel(GuiGraphics guiGraphics) {
+		RenderSystem.setShaderColor(1, 1, 1, 1);
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		guiGraphics.pose().pushPose();
+		guiGraphics.pose().translate(this.leftPos, this.topPos, 0);
+		guiGraphics.pose().scale((float) guiScale, (float) guiScale, 1.0F);
+		guiGraphics.blit(BACKGROUND, 0, 0, 0, 0, TEX_W, TEX_H, TEX_W, TEX_H);
+		guiGraphics.blit(SCREEN_OVERLAY, 0, 0, 0, 0, TEX_W, TEX_H, TEX_W, TEX_H);
+		guiGraphics.blit(hasCoinInserted() ? COIN_ON : COIN_OFF, 0, 0, 0, 0, TEX_W, TEX_H, TEX_W, TEX_H);
+		renderOrderText(guiGraphics);
+		guiGraphics.pose().popPose();
+		RenderSystem.disableBlend();
+	}
+
+	private void renderOrderText(GuiGraphics guiGraphics) {
+		String visible = order.isBlank() && !inputFocused ? "ENTER YOUR ORDER" : order;
+		if (visible.length() > 38) {
+			visible = visible.substring(Math.max(0, visible.length() - 38));
+		}
+		int color = inputFocused ? 0xD8F0FF : 0x8EA8B8;
+		String cursor = inputFocused && (System.currentTimeMillis() / 400L) % 2L == 0L ? "_" : "";
+		guiGraphics.drawString(this.font, visible + cursor, 112, 252, color, false);
+	}
+
+	private void renderHoverTooltip(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+		double tx = textureX(mouseX);
+		double ty = textureY(mouseY);
+		Component tooltip = null;
+		if (contains(COIN_POLY, tx, ty)) {
+			tooltip = Component.literal("Coin");
+		} else if (contains(INPUT_POLY, tx, ty)) {
+			tooltip = Component.literal("Input");
+		} else if (contains(ENTER_POLY, tx, ty)) {
+			tooltip = Component.literal("Enter");
+		}
+		if (tooltip != null) {
+			guiGraphics.renderTooltip(this.font, tooltip, mouseX, mouseY);
+		}
 	}
 
 	@Override
 	protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int gx, int gy) {
-		RenderSystem.setShaderColor(1, 1, 1, 1);
-		RenderSystem.enableBlend();
-		RenderSystem.defaultBlendFunc();
-		guiGraphics.blit(texture, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
-		RenderSystem.disableBlend();
+	}
+
+	@Override
+	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+	}
+
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		if (button != 0) {
+			return true;
+		}
+		double tx = textureX(mouseX);
+		double ty = textureY(mouseY);
+		if (contains(COIN_POLY, tx, ty)) {
+			ScpAdditionsMod.PACKET_HANDLER.sendToServer(new Scp294GuiButtonMessage(1, x, y, z, ""));
+			inputFocused = false;
+			return true;
+		}
+		if (contains(INPUT_POLY, tx, ty)) {
+			inputFocused = true;
+			return true;
+		}
+		if (contains(ENTER_POLY, tx, ty)) {
+			sendDrinkRequestAndClose();
+			return true;
+		}
+		inputFocused = false;
+		return true;
+	}
+
+	@Override
+	public boolean charTyped(char codePoint, int modifiers) {
+		if (inputFocused && StringUtil.isAllowedChatCharacter(codePoint) && order.length() < 80) {
+			order += codePoint;
+			return true;
+		}
+		return true;
 	}
 
 	@Override
@@ -59,64 +141,62 @@ public class Scp294GuiScreen extends AbstractContainerScreen<Scp294GuiMenu> {
 			this.minecraft.player.closeContainer();
 			return true;
 		}
-		if ((key == 257 || key == 335) && scp294input.isFocused()) {
+		if (key == 257 || key == 335) {
 			sendDrinkRequestAndClose();
 			return true;
 		}
-		if (scp294input.isFocused())
-			return scp294input.keyPressed(key, b, c);
-		return super.keyPressed(key, b, c);
-	}
-
-	@Override
-	public void containerTick() {
-		super.containerTick();
-		scp294input.tick();
-	}
-
-	@Override
-	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-		guiGraphics.drawString(this.font, Component.translatable("gui.scp_additions.scp_294_gui.label_scp294"), 68, 8, -12829636, false);
-		guiGraphics.drawString(this.font, Component.translatable("gui.scp_additions.scp_294_gui.label_coin"), 139, 12, -12829636, false);
-	}
-
-	@Override
-	public void onClose() {
-		super.onClose();
-	}
-
-	@Override
-	public void init() {
-		super.init();
-		scp294input = new EditBox(this.font, this.leftPos + 16, this.topPos + 26, 118, 18, Component.translatable("gui.scp_additions.scp_294_gui.scp294input")) {
-			@Override
-			public void insertText(String text) {
-				super.insertText(text);
-				if (getValue().isEmpty())
-					setSuggestion(Component.translatable("gui.scp_additions.scp_294_gui.scp294input").getString());
-				else
-					setSuggestion(null);
+		if (inputFocused && key == 259 && !order.isEmpty()) {
+			order = order.substring(0, order.length() - 1);
+			return true;
+		}
+		if (inputFocused && isPaste(key, c)) {
+			String clipboard = this.minecraft.keyboardHandler.getClipboard();
+			for (char ch : clipboard.toCharArray()) {
+				if (StringUtil.isAllowedChatCharacter(ch) && order.length() < 80) {
+					order += ch;
+				}
 			}
+			return true;
+		}
+		return true;
+	}
 
-			@Override
-			public void moveCursorTo(int pos) {
-				super.moveCursorTo(pos);
-				if (getValue().isEmpty())
-					setSuggestion(Component.translatable("gui.scp_additions.scp_294_gui.scp294input").getString());
-				else
-					setSuggestion(null);
-			}
-		};
-		scp294input.setSuggestion(Component.translatable("gui.scp_additions.scp_294_gui.scp294input").getString());
-		scp294input.setMaxLength(32767);
-		this.addWidget(this.scp294input);
-		button_enter = Button.builder(Component.translatable("gui.scp_additions.scp_294_gui.button_enter"), e -> sendDrinkRequestAndClose())
-				.bounds(this.leftPos + 108, this.topPos + 48, 51, 20).build();
-		this.addRenderableWidget(button_enter);
+	private boolean isPaste(int key, int modifiers) {
+		return key == 86 && hasControlDown();
 	}
 
 	private void sendDrinkRequestAndClose() {
-		ScpAdditionsMod.PACKET_HANDLER.sendToServer(new Scp294GuiButtonMessage(0, x, y, z, scp294input.getValue()));
+		ScpAdditionsMod.PACKET_HANDLER.sendToServer(new Scp294GuiButtonMessage(0, x, y, z, order));
 		this.minecraft.player.closeContainer();
+	}
+
+	private boolean hasCoinInserted() {
+		Slot slot = this.menu.get().get(0);
+		return slot != null && slot.hasItem();
+	}
+
+	private void updateLayout() {
+		this.guiScale = Math.min(1.0D, Math.min((this.width - 20.0D) / TEX_W, (this.height - 20.0D) / TEX_H));
+		this.leftPos = (int) Math.round((this.width - TEX_W * guiScale) / 2.0D);
+		this.topPos = (int) Math.round((this.height - TEX_H * guiScale) / 2.0D);
+	}
+
+	private double textureX(double mouseX) {
+		return (mouseX - this.leftPos) / guiScale;
+	}
+
+	private double textureY(double mouseY) {
+		return (mouseY - this.topPos) / guiScale;
+	}
+
+	private static boolean contains(double[][] polygon, double x, double y) {
+		boolean inside = false;
+		for (int i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+			double xi = polygon[i][0], yi = polygon[i][1];
+			double xj = polygon[j][0], yj = polygon[j][1];
+			boolean intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / ((yj - yi) == 0 ? 0.00001D : (yj - yi)) + xi);
+			if (intersect) inside = !inside;
+		}
+		return inside;
 	}
 }

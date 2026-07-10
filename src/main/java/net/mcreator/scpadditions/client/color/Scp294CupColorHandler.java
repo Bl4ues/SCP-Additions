a27975b2b55804c23a8c3b93144e493e2f06d2f8
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 01:13:06 -0300

    Add SCP-294 dynamic cup color handler

---
+++ src/main/java/net/mcreator/scpadditions/client/color/Scp294CupColorHandler.java
@@ -0,0 +1,30 @@
+package net.mcreator.scpadditions.client.color;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import net.minecraft.nbt.Tag;
+
+import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.init.ScpAdditionsModItems;
+
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public final class Scp294CupColorHandler {
+	private Scp294CupColorHandler() {
+	}
+
+	@SubscribeEvent
+	public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
+		event.register((stack, tintIndex) -> {
+			if (tintIndex != 1) {
+				return 0xFFFFFF;
+			}
+			if (stack.hasTag() && stack.getTag().contains("Scp294Drink", Tag.TAG_COMPOUND)) {
+				return stack.getTag().getCompound("Scp294Drink").getInt("cup_color") & 0xFFFFFF;
+			}
+			return 0xFFFFFF;
+		}, ScpAdditionsModItems.CUP_OF_COFFEE.get());
+	}
+}

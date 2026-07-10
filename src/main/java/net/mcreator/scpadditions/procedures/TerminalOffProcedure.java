82dbcfc809f60050c535d4f5469c1fbd438a950a
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 06:59:00 -0300

    Require credentials to disable Tesla Gates

--- src/main/java/net/mcreator/scpadditions/procedures/TerminalOffProcedure.java
+++ src/main/java/net/mcreator/scpadditions/procedures/TerminalOffProcedure.java
@@ -1,24 +1,14 @@
 package net.mcreator.scpadditions.procedures;
 
-import net.minecraftforge.registries.ForgeRegistries;
-
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.BlockPos;
-
-import net.mcreator.scpadditions.init.ScpAdditionsModGameRules;
 
 public class TerminalOffProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (world instanceof Level _level) {
-			if (!_level.isClientSide()) {
-				_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:click")), SoundSource.NEUTRAL, 1, 1);
-			} else {
-				_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:click")), SoundSource.NEUTRAL, 1, 1, false);
-			}
-		}
-		world.getLevelData().getGameRules().getRule(ScpAdditionsModGameRules.TESLAGATEON).set(false, world.getServer());
+		TeslaTerminalController.disableTeslaGates(world, x, y, z, null);
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Player player) {
+		TeslaTerminalController.disableTeslaGates(world, x, y, z, player);
 	}
 }

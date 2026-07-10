e153f96c8803548b3fc99cbbe64d0d1b161f69b8
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 05:05:28 -0300

    Prevent Tesla Gate queued activation from recreating broken gates

--- src/main/java/net/mcreator/scpadditions/procedures/TeslaGateUpdateTickProcedure.java
+++ src/main/java/net/mcreator/scpadditions/procedures/TeslaGateUpdateTickProcedure.java
@@ -2,57 +2,40 @@ package net.mcreator.scpadditions.procedures;
 
 import net.minecraftforge.registries.ForgeRegistries;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
-import net.mcreator.scpadditions.init.ScpAdditionsModGameRules;
-import net.mcreator.scpadditions.init.ScpAdditionsModBlocks;
 import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.init.ScpAdditionsModBlocks;
+import net.mcreator.scpadditions.init.ScpAdditionsModGameRules;
 
-import java.util.Map;
-import java.util.List;
 import java.util.Comparator;
+import java.util.List;
 
 public class TeslaGateUpdateTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (world.getLevelData().getGameRules().getBoolean(ScpAdditionsModGameRules.TESLAGATEON) == true) {
-			{
-				final Vec3 _center = new Vec3(x, y, z);
-				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-				for (Entity entityiterator : _entfound) {
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:teslaactivate")), SoundSource.HOSTILE, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:teslaactivate")), SoundSource.HOSTILE, 1, 1, false);
-						}
-					}
-					ScpAdditionsMod.queueServerWork(5, () -> {
-						{
-							BlockPos _bp = BlockPos.containing(x, y, z);
-							BlockState _bs = ScpAdditionsModBlocks.TESLA_ACTIVE.get().defaultBlockState();
-							BlockState _bso = world.getBlockState(_bp);
-							for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-								Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-								if (_property != null && _bs.getValue(_property) != null)
-									try {
-										_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-									} catch (Exception e) {
-									}
-							}
-							world.setBlock(_bp, _bs, 3);
-						}
-					});
+		if (!world.getLevelData().getGameRules().getBoolean(ScpAdditionsModGameRules.TESLAGATEON)) {
+			return;
+		}
+
+		final Vec3 center = new Vec3(x, y, z);
+		List<Entity> entities = world.getEntitiesOfClass(Entity.class, new AABB(center, center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(entity -> entity.distanceToSqr(center))).toList();
+		for (Entity ignored : entities) {
+			if (world instanceof Level level) {
+				if (!level.isClientSide()) {
+					level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:teslaactivate")), SoundSource.HOSTILE, 1, 1);
+				} else {
+					level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("scp_additions:teslaactivate")), SoundSource.HOSTILE, 1, 1, false);
 				}
 			}
+			ScpAdditionsMod.queueServerWork(5, () -> TeslaGateTransitionHelper.transitionIfCurrent(world, x, y, z, ScpAdditionsModBlocks.TESLA_GATE, ScpAdditionsModBlocks.TESLA_ACTIVE));
+			break;
 		}
 	}
 }

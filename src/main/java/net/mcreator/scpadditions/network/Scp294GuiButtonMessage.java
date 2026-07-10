b599ce440823949686b637255882617a1c3f3a66
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-09 23:51:05 -0300

    Send SCP-294 input text through packet

--- src/main/java/net/mcreator/scpadditions/network/Scp294GuiButtonMessage.java
+++ src/main/java/net/mcreator/scpadditions/network/Scp294GuiButtonMessage.java
@@ -1,4 +1,3 @@
-
 package net.mcreator.scpadditions.network;
 
 import net.minecraftforge.network.NetworkEvent;
@@ -11,29 +10,30 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.scpadditions.world.inventory.Scp294GuiMenu;
 import net.mcreator.scpadditions.procedures.Scp294drinkGiveProcedure;
 import net.mcreator.scpadditions.ScpAdditionsMod;
 
 import java.util.function.Supplier;
-import java.util.HashMap;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Scp294GuiButtonMessage {
 	private final int buttonID, x, y, z;
+	private final String input;
 
 	public Scp294GuiButtonMessage(FriendlyByteBuf buffer) {
 		this.buttonID = buffer.readInt();
 		this.x = buffer.readInt();
 		this.y = buffer.readInt();
 		this.z = buffer.readInt();
+		this.input = buffer.readUtf(32767);
 	}
 
-	public Scp294GuiButtonMessage(int buttonID, int x, int y, int z) {
+	public Scp294GuiButtonMessage(int buttonID, int x, int y, int z, String input) {
 		this.buttonID = buttonID;
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.input = input;
 	}
 
 	public static void buffer(Scp294GuiButtonMessage message, FriendlyByteBuf buffer) {
@@ -41,30 +41,27 @@ public class Scp294GuiButtonMessage {
 		buffer.writeInt(message.x);
 		buffer.writeInt(message.y);
 		buffer.writeInt(message.z);
+		buffer.writeUtf(message.input == null ? "" : message.input, 32767);
 	}
 
 	public static void handler(Scp294GuiButtonMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
 		NetworkEvent.Context context = contextSupplier.get();
 		context.enqueueWork(() -> {
 			Player entity = context.getSender();
-			int buttonID = message.buttonID;
-			int x = message.x;
-			int y = message.y;
-			int z = message.z;
-			handleButtonAction(entity, buttonID, x, y, z);
+			if (entity != null) {
+				handleButtonAction(entity, message.buttonID, message.x, message.y, message.z, message.input);
+			}
 		});
 		context.setPacketHandled(true);
 	}
 
-	public static void handleButtonAction(Player entity, int buttonID, int x, int y, int z) {
+	public static void handleButtonAction(Player entity, int buttonID, int x, int y, int z, String input) {
 		Level world = entity.level();
-		HashMap guistate = Scp294GuiMenu.guistate;
 		// security measure to prevent arbitrary chunk generation
 		if (!world.hasChunkAt(new BlockPos(x, y, z)))
 			return;
 		if (buttonID == 0) {
-
-			Scp294drinkGiveProcedure.execute(world, x, y, z, entity, guistate);
+			Scp294drinkGiveProcedure.execute(world, x, y, z, entity, input);
 		}
 	}
 

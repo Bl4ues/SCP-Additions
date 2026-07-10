bc3396ceb4435883381a48c3a49bdd347824e2bb
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 05:06:43 -0300

    Use safe Tesla Gate transition for final active pulse

--- src/main/java/net/mcreator/scpadditions/procedures/TeslaActive4BProcedure.java
+++ src/main/java/net/mcreator/scpadditions/procedures/TeslaActive4BProcedure.java
@@ -1,81 +1,11 @@
 package net.mcreator.scpadditions.procedures;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.core.BlockPos;
-import net.minecraft.advancements.AdvancementProgress;
-import net.minecraft.advancements.Advancement;
 
 import net.mcreator.scpadditions.init.ScpAdditionsModBlocks;
-import net.mcreator.scpadditions.ScpAdditionsMod;
-
-import java.util.Map;
-import java.util.List;
-import java.util.Comparator;
 
 public class TeslaActive4BProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		{
-			final Vec3 _center = new Vec3(x, y, z);
-			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-			for (Entity entityiterator : _entfound) {
-				if (entityiterator instanceof LivingEntity _entity)
-					_entity.hurt(new DamageSource(_entity.level().registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)) {
-						@Override
-						public Component getLocalizedDeathMessage(LivingEntity _msgEntity) {
-							String _translatekey = "death.attack." + "teslagate";
-							if (this.getEntity() == null && this.getDirectEntity() == null) {
-								return _msgEntity.getKillCredit() != null
-										? Component.translatable(_translatekey + ".player", _msgEntity.getDisplayName(), _msgEntity.getKillCredit().getDisplayName())
-										: Component.translatable(_translatekey, _msgEntity.getDisplayName());
-							} else {
-								Component _component = this.getEntity() == null ? this.getDirectEntity().getDisplayName() : this.getEntity().getDisplayName();
-								ItemStack _itemstack = ItemStack.EMPTY;
-								if (this.getEntity() instanceof LivingEntity _livingentity)
-									_itemstack = _livingentity.getMainHandItem();
-								return !_itemstack.isEmpty() && _itemstack.hasCustomHoverName()
-										? Component.translatable(_translatekey + ".item", _msgEntity.getDisplayName(), _component, _itemstack.getDisplayName())
-										: Component.translatable(_translatekey, _msgEntity.getDisplayName(), _component);
-							}
-						}
-					}, 20);
-				if (entityiterator instanceof ServerPlayer _player) {
-					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("scp_additions:tesla"));
-					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-					if (!_ap.isDone()) {
-						for (String criteria : _ap.getRemainingCriteria())
-							_player.getAdvancements().award(_adv, criteria);
-					}
-				}
-			}
-		}
-		ScpAdditionsMod.queueServerWork(3, () -> {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = ScpAdditionsModBlocks.TESLA_RECHARGE.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-					if (_property != null && _bs.getValue(_property) != null)
-						try {
-							_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-		});
+		TeslaGatePulseHelper.pulseAndTransition(world, x, y, z, ScpAdditionsModBlocks.TESLA_ACTIVE_4, ScpAdditionsModBlocks.TESLA_RECHARGE);
 	}
 }

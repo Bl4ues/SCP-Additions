7ec879b4b0410e9fb26757009b63ea9db1b55d8a
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 05:29:53 -0300

    Remove Tesla Gate floor collision bar

--- src/main/java/net/mcreator/scpadditions/block/TeslaGateShapeHelper.java
+++ src/main/java/net/mcreator/scpadditions/block/TeslaGateShapeHelper.java
@@ -9,13 +9,11 @@ public final class TeslaGateShapeHelper {
 	private static final VoxelShape NORTH_SOUTH = Shapes.or(
 			Block.box(-8, -16, -2, -5, 32, 18),
 			Block.box(21, -16, -2, 24, 32, 18),
-			Block.box(-8, 27, -2, 24, 32, 18),
-			Block.box(-8, -16, -2, 24, -13, 18));
+			Block.box(-8, 27, -2, 24, 32, 18));
 	private static final VoxelShape EAST_WEST = Shapes.or(
 			Block.box(-2, -16, -8, 18, 32, -5),
 			Block.box(-2, -16, 21, 18, 32, 24),
-			Block.box(-2, 27, -8, 18, 32, 24),
-			Block.box(-2, -16, -8, 18, -13, 24));
+			Block.box(-2, 27, -8, 18, 32, 24));
 
 	private TeslaGateShapeHelper() {
 	}

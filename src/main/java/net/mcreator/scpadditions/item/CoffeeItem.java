f798f7e2789ce983dfeb93b6c1bc7100a19ada9a
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-10 01:12:57 -0300

    Make SCP-294 coffee item dynamic

--- src/main/java/net/mcreator/scpadditions/item/CoffeeItem.java
+++ src/main/java/net/mcreator/scpadditions/item/CoffeeItem.java
@@ -1,25 +1,27 @@
-
 package net.mcreator.scpadditions.item;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
 
-import net.mcreator.scpadditions.procedures.CoffeePlayerFinishesUsingItemProcedure;
 import net.mcreator.scpadditions.init.ScpAdditionsModItems;
 
 import java.util.List;
 
 public class CoffeeItem extends Item {
 	public CoffeeItem() {
-		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f).alwaysEat().build()));
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(0).saturationMod(0.0f).alwaysEat().build()));
 	}
 
 	@Override
@@ -29,7 +31,19 @@ public class CoffeeItem extends Item {
 
 	@Override
 	public int getUseDuration(ItemStack itemstack) {
-		return 40;
+		return 32;
+	}
+
+	@Override
+	public Component getName(ItemStack stack) {
+		if (stack.hasTag() && stack.getTag().contains("Scp294Drink", Tag.TAG_COMPOUND)) {
+			String id = stack.getTag().getCompound("Scp294Drink").getString("id");
+			if (!id.isBlank()) {
+				String path = id.contains(":") ? id.substring(id.indexOf(':') + 1) : id;
+				return Component.literal("Cup of " + toTitleCase(path.replace('_', ' ')));
+			}
+		}
+		return Component.translatable(this.getDescriptionId(stack));
 	}
 
 	@Override
@@ -38,21 +52,41 @@ public class CoffeeItem extends Item {
 	}
 
 	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+		return ItemUtils.startUsingInstantly(world, player, hand);
+	}
+
+	@Override
 	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
-		ItemStack retval = new ItemStack(ScpAdditionsModItems.EMPTY_CUP.get());
 		super.finishUsingItem(itemstack, world, entity);
-		double x = entity.getX();
-		double y = entity.getY();
-		double z = entity.getZ();
-		CoffeePlayerFinishesUsingItemProcedure.execute(entity);
-		if (itemstack.isEmpty()) {
-			return retval;
-		} else {
-			if (entity instanceof Player player && !player.getAbilities().instabuild) {
-				if (!player.getInventory().add(retval))
-					player.drop(retval, false);
+		ItemStack emptyCup = new ItemStack(ScpAdditionsModItems.EMPTY_CUP.get());
+
+		if (entity instanceof Player player && !player.getAbilities().instabuild) {
+			if (itemstack.isEmpty()) {
+				return emptyCup;
 			}
-			return itemstack;
+			if (!player.getInventory().add(emptyCup)) {
+				player.drop(emptyCup, false);
+			}
 		}
+
+		return itemstack;
+	}
+
+	private static String toTitleCase(String value) {
+		StringBuilder result = new StringBuilder();
+		for (String word : value.split(" ")) {
+			if (word.isBlank()) {
+				continue;
+			}
+			if (!result.isEmpty()) {
+				result.append(' ');
+			}
+			result.append(Character.toUpperCase(word.charAt(0)));
+			if (word.length() > 1) {
+				result.append(word.substring(1));
+			}
+		}
+		return result.toString();
 	}
 }

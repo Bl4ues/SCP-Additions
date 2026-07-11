082b3ad50d04c19bc136fa3a623978c4aede2022
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:47:57 -0300

    feat(scp173): register Eye Sore effect

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModMobEffects.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModMobEffects.java
@@ -2,11 +2,14 @@ package net.mcreator.scpadditions.init;
 
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 import net.minecraft.world.effect.MobEffect;
 
 import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.effect.EyeSoreEffect;
 
 public class ScpAdditionsModMobEffects {
 	public static final DeferredRegister<MobEffect> REGISTRY = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ScpAdditionsMod.MODID);
+	public static final RegistryObject<MobEffect> EYE_SORE = REGISTRY.register("eye_sore", EyeSoreEffect::new);
 }

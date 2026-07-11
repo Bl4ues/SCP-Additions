9b2cb5edef1209144fc1100b4185c0a6a27d4325
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:51:07 -0300

    feat(scp173): register manual blink key

---
+++ src/main/java/net/mcreator/scpadditions/client/Scp173Keybinds.java
@@ -0,0 +1,19 @@
+package net.mcreator.scpadditions.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public final class Scp173Keybinds {
+    public static final KeyMapping BLINK = new KeyMapping(
+            "key.scpinventory.blink",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_SPACE,
+            "key.categories.scpinventory"
+    );
+
+    private Scp173Keybinds() {
+    }
+}

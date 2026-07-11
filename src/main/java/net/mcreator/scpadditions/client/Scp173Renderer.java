5740ed4dc5fa805a293f68b07f8446fddeb03b76
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:52:02 -0300

    feat(scp173): migrate GeckoLib model and renderer

---
+++ src/main/java/net/mcreator/scpadditions/client/Scp173Renderer.java
@@ -0,0 +1,21 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.mcreator.scpadditions.entity.Scp173Entity;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+
+public class Scp173Renderer extends GeoEntityRenderer<Scp173Entity> {
+    public Scp173Renderer(EntityRendererProvider.Context context) {
+        super(context, new Scp173Model<>());
+        this.shadowRadius = 0.45F;
+    }
+
+    @Override
+    public RenderType getRenderType(Scp173Entity animatable, ResourceLocation texture,
+                                    MultiBufferSource bufferSource, float partialTick) {
+        return RenderType.entityTranslucent(texture);
+    }
+}

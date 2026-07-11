04c11c1e902ec774f78835e84ef51dda45a7ea73
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 01:57:23 -0300

    Register cutout render layers for Tesla gate blocks

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModScreens.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModScreens.java
@@ -10,6 +10,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 
 import net.mcreator.scpadditions.client.gui.TeslaTerminalScreen;
 import net.mcreator.scpadditions.client.gui.Scp914GuiScreen;
@@ -23,6 +25,13 @@ public class ScpAdditionsModScreens {
 			MenuScreens.register(ScpAdditionsModMenus.TESLA_TERMINAL.get(), TeslaTerminalScreen::new);
 			MenuScreens.register(ScpAdditionsModMenus.SCP_914_GUI.get(), Scp914GuiScreen::new);
 			MenuScreens.register(ScpAdditionsModMenus.SCP_294_GUI.get(), Scp294GuiScreen::new);
+
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_GATE.get(), RenderType.cutout());
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_RECHARGE.get(), RenderType.cutout());
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_ACTIVE.get(), RenderType.cutout());
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_ACTIVE_2.get(), RenderType.cutout());
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_ACTIVE_3.get(), RenderType.cutout());
+			ItemBlockRenderTypes.setRenderLayer(ScpAdditionsModBlocks.TESLA_ACTIVE_4.get(), RenderType.cutout());
 		});
 	}
 }

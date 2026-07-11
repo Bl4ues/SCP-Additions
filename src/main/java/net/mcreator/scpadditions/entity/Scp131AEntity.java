590eac731f49cfe122b061c80834707db9d2278c
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:22:33 -0300

    feat(scp131): add SCP-131-A entity

---
+++ src/main/java/net/mcreator/scpadditions/entity/Scp131AEntity.java
@@ -0,0 +1,15 @@
+package net.mcreator.scpadditions.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class Scp131AEntity extends AbstractScp131Entity {
+    public Scp131AEntity(EntityType<? extends Scp131AEntity> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    public String scpName() {
+        return "SCP-131-A";
+    }
+}

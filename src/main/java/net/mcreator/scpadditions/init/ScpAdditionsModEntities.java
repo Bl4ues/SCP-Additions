157f1d67497618417b809852764ceb87eb49f8ab
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:52:20 -0300

    feat(scp173): register entity and attributes

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
@@ -12,6 +12,7 @@ import net.mcreator.scpadditions.ScpAdditionsMod;
 import net.mcreator.scpadditions.entity.AbstractScp131Entity;
 import net.mcreator.scpadditions.entity.Scp131AEntity;
 import net.mcreator.scpadditions.entity.Scp131BEntity;
+import net.mcreator.scpadditions.entity.Scp173Entity;
 
 @Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ScpAdditionsModEntities {
@@ -19,21 +20,20 @@ public class ScpAdditionsModEntities {
 
 	public static final RegistryObject<EntityType<Scp131AEntity>> SCP_131_A = REGISTRY.register("scp_131_a",
 			() -> EntityType.Builder.of(Scp131AEntity::new, MobCategory.CREATURE)
-					.sized(0.70F, 1.00F)
-					.clientTrackingRange(10)
-					.updateInterval(2)
-					.build("scp_131_a"));
+					.sized(0.70F, 1.00F).clientTrackingRange(10).updateInterval(2).build("scp_131_a"));
 
 	public static final RegistryObject<EntityType<Scp131BEntity>> SCP_131_B = REGISTRY.register("scp_131_b",
 			() -> EntityType.Builder.of(Scp131BEntity::new, MobCategory.CREATURE)
-					.sized(0.70F, 1.00F)
-					.clientTrackingRange(10)
-					.updateInterval(2)
-					.build("scp_131_b"));
+					.sized(0.70F, 1.00F).clientTrackingRange(10).updateInterval(2).build("scp_131_b"));
+
+	public static final RegistryObject<EntityType<Scp173Entity>> SCP_173 = REGISTRY.register("scp_173",
+			() -> EntityType.Builder.of(Scp173Entity::new, MobCategory.MONSTER)
+					.sized(0.85F, 1.95F).clientTrackingRange(12).updateInterval(1).build("scp_173"));
 
 	@SubscribeEvent
 	public static void createAttributes(EntityAttributeCreationEvent event) {
 		event.put(SCP_131_A.get(), AbstractScp131Entity.createAttributes().build());
 		event.put(SCP_131_B.get(), AbstractScp131Entity.createAttributes().build());
+		event.put(SCP_173.get(), Scp173Entity.createAttributes().build());
 	}
 }

e6ae0f8fe833e4956f4b6b4a24e1ba8f8cd549b1
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:23:20 -0300

    feat(scp131): register entities and attributes

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
@@ -1,12 +1,39 @@
 package net.mcreator.scpadditions.init;
 
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.DeferredRegister;
-
 import net.minecraft.world.entity.EntityType;
-
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.entity.AbstractScp131Entity;
+import net.mcreator.scpadditions.entity.Scp131AEntity;
+import net.mcreator.scpadditions.entity.Scp131BEntity;
 
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ScpAdditionsModEntities {
 	public static final DeferredRegister<EntityType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, ScpAdditionsMod.MODID);
+
+	public static final RegistryObject<EntityType<Scp131AEntity>> SCP_131_A = REGISTRY.register("scp_131_a",
+			() -> EntityType.Builder.of(Scp131AEntity::new, MobCategory.CREATURE)
+					.sized(0.70F, 1.00F)
+					.clientTrackingRange(10)
+					.updateInterval(2)
+					.build("scp_131_a"));
+
+	public static final RegistryObject<EntityType<Scp131BEntity>> SCP_131_B = REGISTRY.register("scp_131_b",
+			() -> EntityType.Builder.of(Scp131BEntity::new, MobCategory.CREATURE)
+					.sized(0.70F, 1.00F)
+					.clientTrackingRange(10)
+					.updateInterval(2)
+					.build("scp_131_b"));
+
+	@SubscribeEvent
+	public static void createAttributes(EntityAttributeCreationEvent event) {
+		event.put(SCP_131_A.get(), AbstractScp131Entity.createAttributes().build());
+		event.put(SCP_131_B.get(), AbstractScp131Entity.createAttributes().build());
+	}
 }

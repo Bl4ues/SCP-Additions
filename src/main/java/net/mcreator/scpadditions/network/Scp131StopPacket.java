7d81f172eb62888778cdd62d350124e42a13adc0
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 18:43:52 -0300

    feat(scp131): restore hold-G dismiss input

---
+++ src/main/java/net/mcreator/scpadditions/network/Scp131StopPacket.java
@@ -0,0 +1,28 @@
+package net.mcreator.scpadditions.network;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkEvent;
+import net.mcreator.scpadditions.entity.AbstractScp131Entity;
+
+import java.util.function.Supplier;
+
+public final class Scp131StopPacket {
+    public static void encode(Scp131StopPacket message, FriendlyByteBuf buffer) {
+    }
+
+    public static Scp131StopPacket decode(FriendlyByteBuf buffer) {
+        return new Scp131StopPacket();
+    }
+
+    public static void handle(Scp131StopPacket message, Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> {
+            ServerPlayer player = context.getSender();
+            if (player != null && AbstractScp131Entity.stopFollowersFor(player)) {
+                ScpEntityNetwork.showScp131Notice(player, false);
+            }
+        });
+        context.setPacketHandled(true);
+    }
+}

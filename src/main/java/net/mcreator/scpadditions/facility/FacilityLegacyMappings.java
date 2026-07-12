0ba600e92df477f576a17c5e6889b10b3946e727
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 22:11:37 -0300

    feat(facility): remap legacy SCP Unity registry IDs

---
+++ src/main/java/net/mcreator/scpadditions/facility/FacilityLegacyMappings.java
@@ -0,0 +1,39 @@
+package net.mcreator.scpadditions.facility;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.MissingMappingsEvent;
+import net.minecraftforge.registries.RegistryObject;
+import net.mcreator.scpadditions.ScpAdditionsMod;
+
+/**
+ * Migrates worlds and inventories created by the standalone SCP Unity Extra
+ * Blocks mod to the consolidated scp_additions registry IDs.
+ */
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class FacilityLegacyMappings {
+    private FacilityLegacyMappings() {
+    }
+
+    @SubscribeEvent
+    public static void remapLegacyIds(MissingMappingsEvent event) {
+        for (MissingMappingsEvent.Mapping<Block> mapping :
+                event.getMappings(ForgeRegistries.Keys.BLOCKS, FacilityModule.LEGACY_MODID)) {
+            RegistryObject<Block> replacement = FacilityModule.blockByPath(mapping.getKey().getPath());
+            if (replacement != null && replacement.isPresent()) {
+                mapping.remap(replacement.get());
+            }
+        }
+
+        for (MissingMappingsEvent.Mapping<Item> mapping :
+                event.getMappings(ForgeRegistries.Keys.ITEMS, FacilityModule.LEGACY_MODID)) {
+            RegistryObject<Item> replacement = FacilityModule.itemByPath(mapping.getKey().getPath());
+            if (replacement != null && replacement.isPresent()) {
+                mapping.remap(replacement.get());
+            }
+        }
+    }
+}

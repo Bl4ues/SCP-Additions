a45e53eb7956acf987d3fa84f88b14f927a24004
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-12 03:26:24 -0300

    feat(items): mark SCP spawn eggs as epic rarity

--- src/main/java/net/mcreator/scpadditions/init/Scp131Items.java
+++ src/main/java/net/mcreator/scpadditions/init/Scp131Items.java
@@ -1,6 +1,7 @@
 package net.mcreator.scpadditions.init;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,15 +13,15 @@ public final class Scp131Items {
 
     public static final RegistryObject<Item> SCP_131_A_SPAWN_EGG = REGISTRY.register("scp_131_a_spawn_egg", () ->
             new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_131_A, 0xD96724, 0x1F1B18,
-                    new Item.Properties(), "SCP-131-A Spawn Egg", "tooltip.scp_additions.scp_131_a_spawn_egg"));
+                    new Item.Properties().rarity(Rarity.EPIC), "SCP-131-A Spawn Egg", "tooltip.scp_additions.scp_131_a_spawn_egg"));
 
     public static final RegistryObject<Item> SCP_131_B_SPAWN_EGG = REGISTRY.register("scp_131_b_spawn_egg", () ->
             new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_131_B, 0xDDBB45, 0x342B18,
-                    new Item.Properties(), "SCP-131-B Spawn Egg", "tooltip.scp_additions.scp_131_b_spawn_egg"));
+                    new Item.Properties().rarity(Rarity.EPIC), "SCP-131-B Spawn Egg", "tooltip.scp_additions.scp_131_b_spawn_egg"));
 
     public static final RegistryObject<Item> SCP_173_SPAWN_EGG = REGISTRY.register("scp_173_spawn_egg", () ->
             new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_173, 0x8B8B82, 0x4A1712,
-                    new Item.Properties(), "SCP-173 Spawn Egg", "tooltip.scp_additions.scp_173_spawn_egg"));
+                    new Item.Properties().rarity(Rarity.EPIC), "SCP-173 Spawn Egg", "tooltip.scp_additions.scp_173_spawn_egg"));
 
     private Scp131Items() {
     }

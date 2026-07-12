4141af51804601fbf58189b759167879b919c92b
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-11 21:36:40 -0300

    feat(reader): add configuration screen packet

---
+++ src/main/java/net/mcreator/scpadditions/network/KeycardReaderOpenScreenPacket.java
@@ -0,0 +1,37 @@
+package net.mcreator.scpadditions.network;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.network.NetworkEvent;
+import net.mcreator.scpadditions.client.gui.KeycardReaderConfigScreen;
+
+import java.util.function.Supplier;
+
+public final class KeycardReaderOpenScreenPacket {
+    private final BlockPos pos;
+    private final int level;
+
+    public KeycardReaderOpenScreenPacket(BlockPos pos, int level) {
+        this.pos = pos.immutable();
+        this.level = level;
+    }
+
+    public static void encode(KeycardReaderOpenScreenPacket message, FriendlyByteBuf buffer) {
+        buffer.writeBlockPos(message.pos);
+        buffer.writeByte(message.level);
+    }
+
+    public static KeycardReaderOpenScreenPacket decode(FriendlyByteBuf buffer) {
+        return new KeycardReaderOpenScreenPacket(buffer.readBlockPos(), buffer.readUnsignedByte());
+    }
+
+    public static void handle(KeycardReaderOpenScreenPacket message,
+            Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT,
+                () -> () -> KeycardReaderConfigScreen.open(message.pos, message.level)));
+        context.setPacketHandled(true);
+    }
+}

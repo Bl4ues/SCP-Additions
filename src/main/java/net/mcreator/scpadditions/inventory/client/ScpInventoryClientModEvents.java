d899b7e1c35d5c0b18d864b97e0e1682135d6788
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-12 03:39:00 -0300

    feat(inventory): register inventory notice overlay

---
+++ src/main/java/net/mcreator/scpadditions/inventory/client/ScpInventoryClientModEvents.java
@@ -0,0 +1,22 @@
+package net.mcreator.scpadditions.inventory.client;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.inventory.ClientScpInventoryFullOverlay;
+
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID,
+        value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class ScpInventoryClientModEvents {
+    private ScpInventoryClientModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+        event.registerAboveAll("scp_inventory_full",
+                (gui, graphics, partialTick, width, height) ->
+                        ClientScpInventoryFullOverlay.render(graphics));
+    }
+}

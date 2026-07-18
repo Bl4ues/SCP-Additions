47b4f008e11d316e322c811559888f435b0a3f58
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-18 18:40:00 -0300

    Add SCP-012 custom death message

--- src/main/java/net/mcreator/scpadditions/scp012/Scp012Damage.java
+++ src/main/java/net/mcreator/scpadditions/scp012/Scp012Damage.java
@@ -1,11 +1,13 @@
 package net.mcreator.scpadditions.scp012;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.LivingEntity;
 import net.mcreator.scpadditions.ScpAdditionsMod;
 
 public final class Scp012Damage {
@@ -19,6 +21,12 @@ public final class Scp012Damage {
     public static DamageSource source(ServerLevel level) {
         return new DamageSource(level.registryAccess()
                 .registryOrThrow(Registries.DAMAGE_TYPE)
-                .getHolderOrThrow(TYPE));
+                .getHolderOrThrow(TYPE)) {
+            @Override
+            public Component getLocalizedDeathMessage(LivingEntity entity) {
+                return Component.literal(entity.getDisplayName().getString()
+                        + " could not complete SCP-012");
+            }
+        };
     }
 }

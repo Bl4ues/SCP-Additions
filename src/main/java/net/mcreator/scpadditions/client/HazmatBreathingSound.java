49541f6823786af60004771dbfbd728fc8012cff
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-18 01:08:04 -0300

    Add local Hazmat breathing loop sound

---
+++ src/main/java/net/mcreator/scpadditions/client/HazmatBreathingSound.java
@@ -0,0 +1,36 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.mcreator.scpadditions.equipment.HazmatSuitAccess;
+import net.mcreator.scpadditions.init.ScpAdditionsModSounds;
+
+/** Local, non-positional breathing loop heard from inside the sealed mask. */
+public final class HazmatBreathingSound extends AbstractTickableSoundInstance {
+    public HazmatBreathingSound() {
+        super(ScpAdditionsModSounds.HAZMAT_BREATHING.get(),
+                SoundSource.PLAYERS, RandomSource.create());
+        this.looping = true;
+        this.delay = 0;
+        this.volume = 1.0F;
+        this.pitch = 1.0F;
+        this.relative = true;
+        this.attenuation = SoundInstance.Attenuation.NONE;
+    }
+
+    @Override
+    public void tick() {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.player == null || !minecraft.player.isAlive()
+                || !HazmatSuitAccess.isFullyEquipped(minecraft.player)) {
+            stop();
+        }
+    }
+
+    public void finish() {
+        stop();
+    }
+}

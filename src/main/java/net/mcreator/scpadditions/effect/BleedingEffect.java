fc13bacd6a0eb1afb981a6d56096a78fa3a867c5
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-19 15:37:33 -0300

    Show Bleeding only in inventory interfaces

--- src/main/java/net/mcreator/scpadditions/effect/BleedingEffect.java
+++ src/main/java/net/mcreator/scpadditions/effect/BleedingEffect.java
@@ -1,10 +1,9 @@
 package net.mcreator.scpadditions.effect;
 
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
 /** Persistent blood loss caused by sufficiently severe SCP-012 exposure. */
-public final class BleedingEffect extends MobEffect {
+public final class BleedingEffect extends InventoryOnlyMobEffect {
     public BleedingEffect() {
         super(MobEffectCategory.HARMFUL, 0xA30F19);
     }

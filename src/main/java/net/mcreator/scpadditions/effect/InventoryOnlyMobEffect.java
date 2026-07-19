84f43b0cb8cd53a0f9cada6c65f647d4ec3b3e9b
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-19 15:37:01 -0300

    Add inventory-only mob effect visibility policy

---
+++ src/main/java/net/mcreator/scpadditions/effect/InventoryOnlyMobEffect.java
@@ -0,0 +1,33 @@
+package net.mcreator.scpadditions.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraftforge.client.extensions.common.IClientMobEffectExtensions;
+
+import java.util.function.Consumer;
+
+/**
+ * Shared visibility policy for conditions that should be listed in inventory
+ * interfaces but should not occupy the vanilla in-world HUD effect strip.
+ */
+public abstract class InventoryOnlyMobEffect extends MobEffect {
+    protected InventoryOnlyMobEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientMobEffectExtensions> consumer) {
+        consumer.accept(new IClientMobEffectExtensions() {
+            @Override
+            public boolean isVisibleInInventory(MobEffectInstance instance) {
+                return true;
+            }
+
+            @Override
+            public boolean isVisibleInGui(MobEffectInstance instance) {
+                return false;
+            }
+        });
+    }
+}

2655c08d0aeffcc57557d9fe5f7a325181964594
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-20 20:24:53 -0300

    Keep NeoForge item snapshots authoritative on clients

--- src/main/java/com/bl4ues/scpinventory/client/ItemConfigClientHandler.java
+++ src/main/java/com/bl4ues/scpinventory/client/ItemConfigClientHandler.java
@@ -17,6 +17,7 @@ public final class ItemConfigClientHandler {
     }
 
     public static void reloadItemConfig() {
-        ScpInventoryConfig.reloadFromDisk();
+        // Keep the synchronized host snapshot authoritative on remote servers.
+        ScpInventoryConfig.reload();
     }
 }

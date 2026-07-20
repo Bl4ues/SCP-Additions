dae15ea7b48d16ead319b4be9d31f5cd33739dea
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-20 19:18:56 -0300

    Fix early gamerule registration on NeoForge

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModGameRules.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModGameRules.java
@@ -1,4 +1,3 @@
-
 /*
  *    MCreator note: This file will be REGENERATED on each build.
  */
@@ -6,9 +5,24 @@ package net.mcreator.scpadditions.init;
 
 import net.minecraft.world.level.GameRules;
 
-public class ScpAdditionsModGameRules {
-	public static final GameRules.Key<GameRules.BooleanValue> TESLAGATEON = GameRules.register("teslaGateOn", GameRules.Category.MISC, GameRules.BooleanValue.create(true));
-	public static final GameRules.Key<GameRules.BooleanValue> TESLAGATEMANUALOVERRIDE = GameRules.register("teslaGateManualOverride", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
-	public static final GameRules.Key<GameRules.BooleanValue> SCP079CONTROLON = GameRules.register("scp079controlOn", GameRules.Category.PLAYER, GameRules.BooleanValue.create(false));
-	public static final GameRules.Key<GameRules.BooleanValue> DECONCHECKPOINT = GameRules.register("deconCheckpoint", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
+public final class ScpAdditionsModGameRules {
+    public static final GameRules.Key<GameRules.BooleanValue> TESLAGATEON =
+            GameRules.register("teslaGateOn", GameRules.Category.MISC,
+                    GameRules.BooleanValue.create(true));
+    public static final GameRules.Key<GameRules.BooleanValue> TESLAGATEMANUALOVERRIDE =
+            GameRules.register("teslaGateManualOverride", GameRules.Category.MISC,
+                    GameRules.BooleanValue.create(false));
+    public static final GameRules.Key<GameRules.BooleanValue> SCP079CONTROLON =
+            GameRules.register("scp079controlOn", GameRules.Category.PLAYER,
+                    GameRules.BooleanValue.create(false));
+    public static final GameRules.Key<GameRules.BooleanValue> DECONCHECKPOINT =
+            GameRules.register("deconCheckpoint", GameRules.Category.MISC,
+                    GameRules.BooleanValue.create(false));
+
+    private ScpAdditionsModGameRules() {
+    }
+
+    /** Forces registration before any world copies the global gamerule table. */
+    public static void bootstrap() {
+    }
 }

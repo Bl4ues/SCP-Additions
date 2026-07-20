316a01b9d57920843cbb0d8f5de7d6cee3668edb
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-19 23:57:50 -0300

    Use attachment lifecycle for SCP inventory data

--- src/main/java/com/bl4ues/scpinventory/events/CapabilityEvents.java
+++ src/main/java/com/bl4ues/scpinventory/events/CapabilityEvents.java
@@ -1,45 +1,11 @@
 package com.bl4ues.scpinventory.events;
 
-import net.neoforged.fml.common.EventBusSubscriber;
-
-import com.bl4ues.scpinventory.capability.ScpInventoryCapability;
-import com.bl4ues.scpinventory.capability.ScpInventoryProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-
-@EventBusSubscriber(modid = "scp_additions")
-public class CapabilityEvents {
-
-    private static final ResourceLocation ID =
-            ResourceLocation.fromNamespaceAndPath("scpinventory", "scp_inventory");
-
-    @SubscribeEvent
-    public static void attachCapability(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof Player) {
-            event.addCapability(ID, new ScpInventoryProvider());
-        }
-    }
-
-    @SubscribeEvent
-    public static void clone(PlayerEvent.Clone event) {
-
-        event.getOriginal().reviveCaps();
-
-        event.getOriginal().getCapability(ScpInventoryCapability.INSTANCE).ifPresent(oldCap -> {
-
-            event.getEntity().getCapability(ScpInventoryCapability.INSTANCE).ifPresent(newCap -> {
-
-                newCap.deserializeNBT(oldCap.serializeNBT());
-
-            });
-
-        });
-
-        event.getOriginal().invalidateCaps();
+/**
+ * NeoForge data attachments own inventory persistence and clone handling.
+ * The registered attachment is serializable and copy-on-death, replacing the
+ * old AttachCapabilitiesEvent and manual PlayerEvent.Clone implementation.
+ */
+public final class CapabilityEvents {
+    private CapabilityEvents() {
     }
 }

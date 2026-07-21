8f9b9539d148688213c95dc6d30b6203c6f403fc
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-21 17:22:25 -0300

    Define roamer scheduler results

---
+++ src/main/java/net/mcreator/scpadditions/roamer/RoamerResult.java
@@ -0,0 +1,16 @@
+package net.mcreator.scpadditions.roamer;
+
+/** Most recent scheduler event, kept separately from the current roamer state. */
+public enum RoamerResult {
+    NONE,
+    TIMER_STARTED,
+    SPAWNED,
+    DESPAWNED_TIMER_RESET,
+    CHANCE_FAILED,
+    NO_VALID_POSITION,
+    BLOCKED_BY_EXISTING,
+    RULE_DISABLED,
+    NOT_IMPLEMENTED,
+    PAUSED_CREATIVE,
+    PAUSED_SPECTATOR
+}

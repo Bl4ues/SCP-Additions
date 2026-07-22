12c04e673227310098b91f30e03a5b085118983d
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-22 07:39:17 -0300

    Use decontamination multiblock collision shell

--- src/main/java/net/mcreator/scpadditions/block/DeconOpenBlock.java
+++ src/main/java/net/mcreator/scpadditions/block/DeconOpenBlock.java
@@ -1,165 +1,37 @@
 package net.mcreator.scpadditions.block;
 
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
-import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.PickaxeItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.util.RandomSource;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
-import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
-
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.mcreator.scpadditions.procedures.DeconOpenUpdateTickProcedure;
 
-import java.util.List;
-import java.util.Collections;
+public class DeconOpenBlock extends AbstractDecontaminationBlock {
+    @Override
+    protected boolean isClosedState() {
+        return false;
+    }
 
-public class DeconOpenBlock extends Block implements SimpleWaterloggedBlock {
-	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    @Override
+    protected boolean raisesOnInitialPlacement() {
+        return true;
+    }
 
-	public DeconOpenBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(20f, 30f).lightLevel(s -> 6).requiresCorrectToolForDrops().noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false));
-	}
+    @Override
+    protected void controllerPlaced(BlockState state, Level level,
+            BlockPos pos, BlockState oldState, boolean moving) {
+        level.scheduleTick(pos, this, 5);
+    }
 
-	@Override
-	public void appendHoverText(ItemStack itemstack, BlockGetter world, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, world, list, flag);
-	}
-
-	@Override
-	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
-		return state.getFluidState().isEmpty();
-	}
-
-	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 0;
-	}
-
-	@Override
-	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return Shapes.empty();
-	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return switch (state.getValue(FACING)) {
-			default -> Shapes.or(box(-16, -16, -16, -13, 32, 32), box(-16, 19, -16, 16, 32, -13), box(13, -16, -16, 16, 32, 32), box(-16, 19, 29, 15, 32, 32), box(-16, -16, 29, -8, 19, 32), box(10, -16, 29, 16, 19, 32),
-					box(10, -16, -16, 16, 19, -13), box(-16, -16, -16, -8, 19, -13), box(10, -16, 29, 16, 19, 32), box(10, -16, -16, 16, 19, 0), box(-16, -16, -16, -8, 19, -13), box(-16, -16, 29, -8, 19, 32), box(-14, 30, -16, 0, 32, 32));
-			case NORTH -> Shapes.or(box(29, -16, -16, 32, 32, 32), box(0, 19, 29, 32, 32, 32), box(0, -16, -16, 3, 32, 32), box(1, 19, -16, 32, 32, -13), box(24, -16, -16, 32, 19, -13), box(0, -16, -16, 6, 19, -13), box(0, -16, 29, 6, 19, 32),
-					box(24, -16, 29, 32, 19, 32), box(0, -16, -16, 6, 19, -13), box(0, -16, 16, 6, 19, 32), box(24, -16, 29, 32, 19, 32), box(24, -16, -16, 32, 19, -13), box(16, 30, -16, 30, 32, 32));
-			case EAST -> Shapes.or(box(-16, -16, 29, 32, 32, 32), box(-16, 19, 0, -13, 32, 32), box(-16, -16, 0, 32, 32, 3), box(29, 19, 1, 32, 32, 32), box(29, -16, 24, 32, 19, 32), box(29, -16, 0, 32, 19, 6), box(-16, -16, 0, -13, 19, 6),
-					box(-16, -16, 24, -13, 19, 32), box(29, -16, 0, 32, 19, 6), box(-16, -16, 0, 0, 19, 6), box(-16, -16, 24, -13, 19, 32), box(29, -16, 24, 32, 19, 32), box(-16, 30, 16, 32, 32, 30));
-			case WEST -> Shapes.or(box(-16, -16, -16, 32, 32, -13), box(29, 19, -16, 32, 32, 16), box(-16, -16, 13, 32, 32, 16), box(-16, 19, -16, -13, 32, 15), box(-16, -16, -16, -13, 19, -8), box(-16, -16, 10, -13, 19, 16),
-					box(29, -16, 10, 32, 19, 16), box(29, -16, -16, 32, 19, -8), box(-16, -16, 10, -13, 19, 16), box(16, -16, 10, 32, 19, 16), box(29, -16, -16, 32, 19, -8), box(-16, -16, -16, -13, 19, -8), box(-16, 30, -14, 32, 32, 0));
-		};
-	}
-
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(FACING, WATERLOGGED);
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
-		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(WATERLOGGED, flag);
-	}
-
-	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
-	}
-
-	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
-	}
-
-	@Override
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-
-	@Override
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
-		if (state.getValue(WATERLOGGED)) {
-			world.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
-		}
-		return super.updateShape(state, facing, facingState, world, currentPos, facingPos);
-	}
-
-	@Override
-	public boolean canHarvestBlock(BlockState state, BlockGetter world, BlockPos pos, Player player) {
-		if (player.getInventory().getSelected().getItem() instanceof PickaxeItem tieredItem)
-			return tieredItem.getTier().getLevel() >= 1;
-		return false;
-	}
-
-	@Override
-	public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
-		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
-		if (!dropsOriginal.isEmpty())
-			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
-	}
-
-	@Override
-	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
-		if (tryRaiseOnPlacement(blockstate, world, pos, oldState, moving)) {
-			return;
-		}
-		super.onPlace(blockstate, world, pos, oldState, moving);
-		world.scheduleTick(pos, this, 5);
-	}
-
-	private boolean tryRaiseOnPlacement(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
-		if (moving || oldState.getBlock() == this || world.isClientSide()) {
-			return false;
-		}
-		if (world.getBlockState(pos.below()).getBlock() == this || !world.getBlockState(pos.below()).isFaceSturdy(world, pos.below(), Direction.UP)) {
-			return false;
-		}
-		BlockPos raisedPos = pos.above();
-		if (!world.getBlockState(raisedPos).isAir()) {
-			return false;
-		}
-		world.setBlock(raisedPos, blockstate, 3);
-		world.setBlock(pos, blockstate.getValue(WATERLOGGED) ? Blocks.WATER.defaultBlockState() : Blocks.AIR.defaultBlockState(), 3);
-		return true;
-	}
-
-	@Override
-	public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
-		super.tick(blockstate, world, pos, random);
-		int x = pos.getX();
-		int y = pos.getY();
-		int z = pos.getZ();
-		DeconOpenUpdateTickProcedure.execute(world, x, y, z);
-		world.scheduleTick(pos, this, 5);
-	}
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos,
+            RandomSource random) {
+        ensureStructure(level, pos, state);
+        DeconOpenUpdateTickProcedure.execute(level,
+                pos.getX(), pos.getY(), pos.getZ());
+        if (level.getBlockState(pos).is(this)) {
+            level.scheduleTick(pos, this, 5);
+        }
+    }
 }

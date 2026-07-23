5d43c5d4fdb90ae852cf2af9cee86c8294c98818
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-22 22:14:33 -0300

    Make SCP-106 corrosion pools more organic

--- src/main/java/net/mcreator/scpadditions/client/Scp106CorrosionParticle.java
+++ src/main/java/net/mcreator/scpadditions/client/Scp106CorrosionParticle.java
@@ -13,8 +13,8 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
 
 public final class Scp106CorrosionParticle extends TextureSheetParticle {
+    private static final int EDGE_SEGMENTS = 14;
     private static final float MAX_ALPHA = 0.78F;
-    private static final float UV_CROP = 0.0F;
 
     private final SpriteSet sprites;
     private final float puddleRotation;
@@ -22,9 +22,10 @@ public final class Scp106CorrosionParticle extends TextureSheetParticle {
     private final float secondLobeAngle;
     private final float firstLobeDistance;
     private final float secondLobeDistance;
+    private final float[] edgeVariation = new float[EDGE_SEGMENTS];
 
-    private Scp106CorrosionParticle(ClientLevel level, double x, double y, double z,
-            SpriteSet sprites) {
+    private Scp106CorrosionParticle(ClientLevel level, double x, double y,
+            double z, SpriteSet sprites) {
         super(level, x, y, z);
         this.sprites = sprites;
         this.xd = 0.0D;
@@ -35,13 +36,21 @@ public final class Scp106CorrosionParticle extends TextureSheetParticle {
         this.hasPhysics = false;
         this.lifetime = 60 + this.random.nextInt(31);
         this.quadSize = 0.34F + this.random.nextFloat() * 0.14F;
-        this.puddleRotation = this.random.nextFloat() * ((float) Math.PI * 2.0F);
+        this.puddleRotation = this.random.nextFloat()
+                * ((float) Math.PI * 2.0F);
         this.firstLobeAngle = this.puddleRotation
                 + 1.75F + this.random.nextFloat() * 0.75F;
         this.secondLobeAngle = this.puddleRotation
                 + 3.85F + this.random.nextFloat() * 0.85F;
-        this.firstLobeDistance = 0.48F + this.random.nextFloat() * 0.20F;
-        this.secondLobeDistance = 0.42F + this.random.nextFloat() * 0.18F;
+        this.firstLobeDistance = 0.48F
+                + this.random.nextFloat() * 0.20F;
+        this.secondLobeDistance = 0.42F
+                + this.random.nextFloat() * 0.18F;
+        for (int i = 0; i < EDGE_SEGMENTS; i++) {
+            float broadWave = Mth.sin(i * 1.73F + puddleRotation) * 0.08F;
+            edgeVariation[i] = 0.82F + broadWave
+                    + this.random.nextFloat() * 0.28F;
+        }
         this.setColor(
                 0.040F + this.random.nextFloat() * 0.030F,
                 0.015F + this.random.nextFloat() * 0.018F,
@@ -53,7 +62,6 @@ public final class Scp106CorrosionParticle extends TextureSheetParticle {
     @Override
     public void tick() {
         super.tick();
-
         float remaining = 1.0F - Mth.clamp(
                 this.age / (float) this.lifetime, 0.0F, 1.0F);
         float fade = Mth.clamp(remaining / 0.32F, 0.0F, 1.0F);
@@ -72,73 +80,90 @@ public final class Scp106CorrosionParticle extends TextureSheetParticle {
         float renderZ = (float) (Mth.lerp(partialTick, this.zo, this.z)
                 - cameraPosition.z());
         float size = getQuadSize(partialTick);
-
-        float fullMinU = getU0();
-        float fullMaxU = getU1();
-        float fullMinV = getV0();
-        float fullMaxV = getV1();
-        float minU = Mth.lerp(UV_CROP, fullMinU, fullMaxU);
-        float maxU = Mth.lerp(1.0F - UV_CROP, fullMinU, fullMaxU);
-        float minV = Mth.lerp(UV_CROP, fullMinV, fullMaxV);
-        float maxV = Mth.lerp(1.0F - UV_CROP, fullMinV, fullMaxV);
         int light = getLightColor(partialTick);
 
-        renderLobe(vertexConsumer,
-                renderX, renderY, renderZ,
-                size * 1.22F, size * 0.82F,
-                puddleRotation,
-                minU, maxU, minV, maxV,
-                light, 1.0F);
-
-        renderLobe(vertexConsumer,
-                renderX + Mth.cos(firstLobeAngle) * size * firstLobeDistance,
+        renderOrganicLobe(vertexConsumer, renderX, renderY, renderZ,
+                size * 1.22F, size * 0.82F, puddleRotation,
+                light, 1.0F, 0);
+        renderOrganicLobe(vertexConsumer,
+                renderX + Mth.cos(firstLobeAngle)
+                        * size * firstLobeDistance,
                 renderY + 0.0004F,
-                renderZ + Mth.sin(firstLobeAngle) * size * firstLobeDistance,
+                renderZ + Mth.sin(firstLobeAngle)
+                        * size * firstLobeDistance,
                 size * 0.76F, size * 0.57F,
                 puddleRotation + 0.68F,
-                minU, maxU, minV, maxV,
-                light, 0.92F);
-
-        renderLobe(vertexConsumer,
-                renderX + Mth.cos(secondLobeAngle) * size * secondLobeDistance,
+                light, 0.92F, 4);
+        renderOrganicLobe(vertexConsumer,
+                renderX + Mth.cos(secondLobeAngle)
+                        * size * secondLobeDistance,
                 renderY + 0.0008F,
-                renderZ + Mth.sin(secondLobeAngle) * size * secondLobeDistance,
+                renderZ + Mth.sin(secondLobeAngle)
+                        * size * secondLobeDistance,
                 size * 0.63F, size * 0.50F,
                 puddleRotation - 0.54F,
-                minU, maxU, minV, maxV,
-                light, 0.86F);
+                light, 0.86F, 9);
     }
 
-    private void renderLobe(VertexConsumer vertexConsumer,
+    private void renderOrganicLobe(VertexConsumer vertexConsumer,
             float centerX, float centerY, float centerZ,
             float radiusX, float radiusZ, float rotation,
-            float minU, float maxU, float minV, float maxV,
-            int light, float alphaMultiplier) {
-        float cos = Mth.cos(rotation);
-        float sin = Mth.sin(rotation);
-
-        float x0 = centerX + (-radiusX * cos - -radiusZ * sin);
-        float z0 = centerZ + (-radiusX * sin + -radiusZ * cos);
-        float x1 = centerX + (-radiusX * cos - radiusZ * sin);
-        float z1 = centerZ + (-radiusX * sin + radiusZ * cos);
-        float x2 = centerX + (radiusX * cos - radiusZ * sin);
-        float z2 = centerZ + (radiusX * sin + radiusZ * cos);
-        float x3 = centerX + (radiusX * cos - -radiusZ * sin);
-        float z3 = centerZ + (radiusX * sin + -radiusZ * cos);
+            int light, float alphaMultiplier, int variationOffset) {
+        float centerU = (getU0() + getU1()) * 0.5F;
+        float centerV = (getV0() + getV1()) * 0.5F;
+        float halfU = (getU1() - getU0()) * 0.5F;
+        float halfV = (getV1() - getV0()) * 0.5F;
         float renderedAlpha = alpha * alphaMultiplier;
+        float cosRotation = Mth.cos(rotation);
+        float sinRotation = Mth.sin(rotation);
 
-        vertexConsumer.vertex(x0, centerY, z0)
-                .uv(maxU, maxV).color(rCol, gCol, bCol, renderedAlpha)
-                .uv2(light).endVertex();
-        vertexConsumer.vertex(x1, centerY, z1)
-                .uv(maxU, minV).color(rCol, gCol, bCol, renderedAlpha)
-                .uv2(light).endVertex();
-        vertexConsumer.vertex(x2, centerY, z2)
-                .uv(minU, minV).color(rCol, gCol, bCol, renderedAlpha)
-                .uv2(light).endVertex();
-        vertexConsumer.vertex(x3, centerY, z3)
-                .uv(minU, maxV).color(rCol, gCol, bCol, renderedAlpha)
-                .uv2(light).endVertex();
+        for (int segment = 0; segment < EDGE_SEGMENTS; segment++) {
+            int next = (segment + 1) % EDGE_SEGMENTS;
+            float angleA = (float) (Math.PI * 2.0D * segment
+                    / EDGE_SEGMENTS);
+            float angleB = (float) (Math.PI * 2.0D * next
+                    / EDGE_SEGMENTS);
+            float variationA = edgeVariation[
+                    (segment + variationOffset) % EDGE_SEGMENTS];
+            float variationB = edgeVariation[
+                    (next + variationOffset) % EDGE_SEGMENTS];
+
+            float localXA = Mth.cos(angleA) * radiusX * variationA;
+            float localZA = Mth.sin(angleA) * radiusZ * variationA;
+            float localXB = Mth.cos(angleB) * radiusX * variationB;
+            float localZB = Mth.sin(angleB) * radiusZ * variationB;
+
+            float edgeAX = centerX
+                    + localXA * cosRotation - localZA * sinRotation;
+            float edgeAZ = centerZ
+                    + localXA * sinRotation + localZA * cosRotation;
+            float edgeBX = centerX
+                    + localXB * cosRotation - localZB * sinRotation;
+            float edgeBZ = centerZ
+                    + localXB * sinRotation + localZB * cosRotation;
+
+            float uA = centerU + Mth.cos(angleA) * halfU;
+            float vA = centerV + Mth.sin(angleA) * halfV;
+            float uB = centerU + Mth.cos(angleB) * halfU;
+            float vB = centerV + Mth.sin(angleB) * halfV;
+
+            vertexConsumer.vertex(centerX, centerY, centerZ)
+                    .uv(centerU, centerV)
+                    .color(rCol, gCol, bCol, renderedAlpha)
+                    .uv2(light).endVertex();
+            vertexConsumer.vertex(edgeAX, centerY, edgeAZ)
+                    .uv(uA, vA)
+                    .color(rCol, gCol, bCol, renderedAlpha)
+                    .uv2(light).endVertex();
+            vertexConsumer.vertex(edgeBX, centerY, edgeBZ)
+                    .uv(uB, vB)
+                    .color(rCol, gCol, bCol, renderedAlpha)
+                    .uv2(light).endVertex();
+            vertexConsumer.vertex(centerX, centerY, centerZ)
+                    .uv(centerU, centerV)
+                    .color(rCol, gCol, bCol, renderedAlpha)
+                    .uv2(light).endVertex();
+        }
     }
 
     @Override

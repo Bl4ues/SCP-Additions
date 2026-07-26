c122c440803c6c75506a1496cd120638d825c728
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-26 16:54:37 -0300

    Add SCP-106 combat and Wither audio
    Add localized SCP-106 footsteps, confirmed melee-hit and ranged-corrosion splash cues, and a smoothly fading positional Wither loop.

--- src/main/java/net/mcreator/scpadditions/init/Scp106Sounds.java
+++ src/main/java/net/mcreator/scpadditions/init/Scp106Sounds.java
@@ -16,6 +16,11 @@ public final class Scp106Sounds {
     public static final RegistryObject<SoundEvent> PHASE = register("scp_106_phase");
     public static final RegistryObject<SoundEvent> CHASE = register("scp_106_chase");
     public static final RegistryObject<SoundEvent> STOP = register("scp_106_stop");
+    public static final RegistryObject<SoundEvent> STEP = register("scp_106_step");
+    public static final RegistryObject<SoundEvent> HIT = register("scp_106_hit");
+    public static final RegistryObject<SoundEvent> RANGED_SPLASH =
+            register("scp_106_ranged_splash");
+    public static final RegistryObject<SoundEvent> WITHER = register("wither");
     public static final RegistryObject<SoundEvent> ENTER = register("enter");
 
     private Scp106Sounds() {

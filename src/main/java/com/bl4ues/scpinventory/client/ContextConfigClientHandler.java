d288c778c0d45142ded79bb0948f6dd2239fdaa3
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-31 14:25:53 -0300

    Open clean context anchor editor

--- src/main/java/com/bl4ues/scpinventory/client/ContextConfigClientHandler.java
+++ src/main/java/com/bl4ues/scpinventory/client/ContextConfigClientHandler.java
@@ -1,6 +1,6 @@
 package com.bl4ues.scpinventory.client;
 
-import com.bl4ues.scpinventory.client.gui.ContextConfigScreen;
+import com.bl4ues.scpinventory.client.gui.ContextAnchorEditorScreen;
 import com.bl4ues.scpinventory.context.ContextInteractionRegistry;
 import com.bl4ues.scpinventory.network.ContextConfigOpenPacket;
 import net.minecraft.client.Minecraft;
@@ -14,7 +14,7 @@ public final class ContextConfigClientHandler {
 
     public static void open(ContextConfigOpenPacket packet) {
         Minecraft mc = Minecraft.getInstance();
-        mc.setScreen(new ContextConfigScreen(packet));
+        mc.setScreen(new ContextAnchorEditorScreen(packet));
     }
 
     public static void reloadContextConfig() {

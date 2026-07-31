15bc7d8382c487e02cdbb553c2bc3148533eba51
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-31 17:04:53 -0300

    Let Screwdrivers remove SCP-131 entities

--- src/main/java/net/mcreator/scpadditions/item/ScrewdriverItem.java
+++ src/main/java/net/mcreator/scpadditions/item/ScrewdriverItem.java
@@ -2,10 +2,15 @@ package net.mcreator.scpadditions.item;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.mcreator.scpadditions.entity.AbstractScp131Entity;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -13,6 +18,19 @@ import java.util.List;
 public final class ScrewdriverItem extends Item {
     public ScrewdriverItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player,
+            LivingEntity target, InteractionHand hand) {
+        if (target instanceof AbstractScp131Entity scp131) {
+            if (!player.level().isClientSide) {
+                scp131.stopFollowing();
+                scp131.discard();
+            }
+            return InteractionResult.sidedSuccess(player.level().isClientSide);
+        }
+        return super.interactLivingEntity(stack, player, target, hand);
     }
 
     @Override

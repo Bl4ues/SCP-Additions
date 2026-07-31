5b231758c68030d0b4b5cb75522ef5e77751cf49
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-31 17:04:05 -0300

    Make SCP-131-A immune to damage

--- src/main/java/net/mcreator/scpadditions/entity/Scp131AEntity.java
+++ src/main/java/net/mcreator/scpadditions/entity/Scp131AEntity.java
@@ -1,11 +1,22 @@
 package net.mcreator.scpadditions.entity;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
 public class Scp131AEntity extends AbstractScp131Entity {
     public Scp131AEntity(EntityType<? extends Scp131AEntity> type, Level level) {
         super(type, level);
+    }
+
+    @Override
+    public boolean isInvulnerableTo(DamageSource source) {
+        return true;
+    }
+
+    @Override
+    public boolean hurt(DamageSource source, float amount) {
+        return false;
     }
 
     @Override

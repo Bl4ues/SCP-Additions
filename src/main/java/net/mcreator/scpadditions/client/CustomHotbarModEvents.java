503c0bee8093b48d9ca74bca9e8fe94153b43044
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-01 14:38:23 -0300

    Register custom hotbar overlay

---
+++ src/main/java/net/mcreator/scpadditions/client/CustomHotbarModEvents.java
@@ -0,0 +1,21 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.mcreator.scpadditions.ScpAdditionsMod;
+
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID,
+        bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public final class CustomHotbarModEvents {
+    private CustomHotbarModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+        event.registerAboveAll("custom_hotbar_overlay",
+                (gui, graphics, partialTick, width, height) ->
+                        CustomHotbarOverlay.render(graphics, width, height));
+    }
+}

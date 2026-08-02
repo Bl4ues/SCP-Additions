5e8dd69e449a1457b2361f352ff2e02abf04d58b
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-01 23:44:54 -0300

    Add construction sign block entity

---
+++ src/main/java/net/mcreator/scpadditions/facility/AreaUnderConstructionSignBlockEntity.java
@@ -0,0 +1,13 @@
+package net.mcreator.scpadditions.facility;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+/** Render-only block entity for the static construction-area notice. */
+public final class AreaUnderConstructionSignBlockEntity extends BlockEntity {
+    public AreaUnderConstructionSignBlockEntity(BlockPos pos,
+            BlockState state) {
+        super(AreaUnderConstructionSignModule.BLOCK_ENTITY, pos, state);
+    }
+}

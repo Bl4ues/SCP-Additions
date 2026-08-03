b9c1cfa37da746bcb48bb2ecbd8f42e864c12042
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-03 17:54:25 -0300

    Move Roomba sound event into main namespace

--- src/main/java/net/mcreator/scpadditions/client/RoombaSounds.java
+++ src/main/java/net/mcreator/scpadditions/client/RoombaSounds.java
@@ -2,11 +2,12 @@ package net.mcreator.scpadditions.client;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.mcreator.scpadditions.ScpAdditionsMod;
 
 /** Client-local sound event used by the continuously tracked Roomba loop. */
 public final class RoombaSounds {
     public static final SoundEvent LOOP = SoundEvent.createVariableRangeEvent(
-            new ResourceLocation("scp_additions_roomba", "loop"));
+            new ResourceLocation(ScpAdditionsMod.MODID, "roomba_loop"));
 
     private RoombaSounds() {
     }

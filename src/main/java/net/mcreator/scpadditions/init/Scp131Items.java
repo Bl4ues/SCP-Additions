25ba0b129220395dc384729e5c865a43a97f3c4b
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-03 14:13:04 -0300

    Add Roomba spawn egg

--- src/main/java/net/mcreator/scpadditions/init/Scp131Items.java
+++ src/main/java/net/mcreator/scpadditions/init/Scp131Items.java
@@ -9,23 +9,54 @@ import net.mcreator.scpadditions.ScpAdditionsMod;
 import net.mcreator.scpadditions.item.NamedSpawnEggItem;
 
 public final class Scp131Items {
-    public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ScpAdditionsMod.MODID);
+    public static final DeferredRegister<Item> REGISTRY =
+            DeferredRegister.create(ForgeRegistries.ITEMS,
+                    ScpAdditionsMod.MODID);
 
-    public static final RegistryObject<Item> SCP_106_SPAWN_EGG = REGISTRY.register("scp_106_spawn_egg", () ->
-            new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_106, 0x0A0A0A, 0x5B3924,
-                    new Item.Properties().rarity(Rarity.EPIC), "SCP-106 Spawn Egg", "tooltip.scp_additions.scp_106_spawn_egg"));
+    public static final RegistryObject<Item> SCP_106_SPAWN_EGG =
+            REGISTRY.register("scp_106_spawn_egg", () ->
+                    new NamedSpawnEggItem(
+                            ScpAdditionsModEntities.SCP_106,
+                            0x0A0A0A, 0x5B3924,
+                            new Item.Properties().rarity(Rarity.EPIC),
+                            "SCP-106 Spawn Egg",
+                            "tooltip.scp_additions.scp_106_spawn_egg"));
 
-    public static final RegistryObject<Item> SCP_131_A_SPAWN_EGG = REGISTRY.register("scp_131_a_spawn_egg", () ->
-            new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_131_A, 0xD96724, 0x1F1B18,
-                    new Item.Properties().rarity(Rarity.EPIC), "SCP-131-A Spawn Egg", "tooltip.scp_additions.scp_131_a_spawn_egg"));
+    public static final RegistryObject<Item> SCP_131_A_SPAWN_EGG =
+            REGISTRY.register("scp_131_a_spawn_egg", () ->
+                    new NamedSpawnEggItem(
+                            ScpAdditionsModEntities.SCP_131_A,
+                            0xD96724, 0x1F1B18,
+                            new Item.Properties().rarity(Rarity.EPIC),
+                            "SCP-131-A Spawn Egg",
+                            "tooltip.scp_additions.scp_131_a_spawn_egg"));
 
-    public static final RegistryObject<Item> SCP_131_B_SPAWN_EGG = REGISTRY.register("scp_131_b_spawn_egg", () ->
-            new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_131_B, 0xDDBB45, 0x342B18,
-                    new Item.Properties().rarity(Rarity.EPIC), "SCP-131-B Spawn Egg", "tooltip.scp_additions.scp_131_b_spawn_egg"));
+    public static final RegistryObject<Item> SCP_131_B_SPAWN_EGG =
+            REGISTRY.register("scp_131_b_spawn_egg", () ->
+                    new NamedSpawnEggItem(
+                            ScpAdditionsModEntities.SCP_131_B,
+                            0xDDBB45, 0x342B18,
+                            new Item.Properties().rarity(Rarity.EPIC),
+                            "SCP-131-B Spawn Egg",
+                            "tooltip.scp_additions.scp_131_b_spawn_egg"));
 
-    public static final RegistryObject<Item> SCP_173_SPAWN_EGG = REGISTRY.register("scp_173_spawn_egg", () ->
-            new NamedSpawnEggItem(ScpAdditionsModEntities.SCP_173, 0x8B8B82, 0x4A1712,
-                    new Item.Properties().rarity(Rarity.EPIC), "SCP-173 Spawn Egg", "tooltip.scp_additions.scp_173_spawn_egg"));
+    public static final RegistryObject<Item> SCP_173_SPAWN_EGG =
+            REGISTRY.register("scp_173_spawn_egg", () ->
+                    new NamedSpawnEggItem(
+                            ScpAdditionsModEntities.SCP_173,
+                            0x8B8B82, 0x4A1712,
+                            new Item.Properties().rarity(Rarity.EPIC),
+                            "SCP-173 Spawn Egg",
+                            "tooltip.scp_additions.scp_173_spawn_egg"));
+
+    public static final RegistryObject<Item> ROOMBA_SPAWN_EGG =
+            REGISTRY.register("roomba_spawn_egg", () ->
+                    new NamedSpawnEggItem(
+                            ScpAdditionsModEntities.ROOMBA,
+                            0x555B5E, 0x3D7FA8,
+                            new Item.Properties().rarity(Rarity.UNCOMMON),
+                            "Roomba Spawn Egg",
+                            "tooltip.scp_additions.roomba_spawn_egg"));
 
     private Scp131Items() {
     }

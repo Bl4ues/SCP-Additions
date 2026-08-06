97615972f0957c85f709ad3cc3777212f8121c06
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-06 00:18:00 -0300

    Register menu music under scp_additions

--- src/main/java/net/mcreator/scpadditions/init/MainMenuSounds.java
+++ src/main/java/net/mcreator/scpadditions/init/MainMenuSounds.java
@@ -5,16 +5,18 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.mcreator.scpadditions.ScpAdditionsMod;
 
 /** Sound registry kept separate from MCreator's regenerated sound holder. */
 public final class MainMenuSounds {
-    public static final String NAMESPACE = "scp_additions_menu";
     public static final DeferredRegister<SoundEvent> REGISTRY =
-            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, NAMESPACE);
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS,
+                    ScpAdditionsMod.MODID);
     public static final RegistryObject<SoundEvent> MAIN_MENU =
             REGISTRY.register("main_menu", () ->
                     SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(NAMESPACE, "main_menu")));
+                            new ResourceLocation(ScpAdditionsMod.MODID,
+                                    "main_menu")));
 
     private MainMenuSounds() {
     }

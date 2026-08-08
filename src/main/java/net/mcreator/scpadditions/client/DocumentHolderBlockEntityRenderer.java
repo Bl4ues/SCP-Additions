9ed0be2bf5781590fdd92c9d124a298c09e79420
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-07 21:03:08 -0300

    Add Document Holder block renderer

---
+++ src/main/java/net/mcreator/scpadditions/client/DocumentHolderBlockEntityRenderer.java
@@ -0,0 +1,24 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.mcreator.scpadditions.facility.DocumentHolderBlockEntity;
+import software.bernie.geckolib.renderer.GeoBlockRenderer;
+
+/** Double-sided renderer for the thin authored Document Holder geometry. */
+public final class DocumentHolderBlockEntityRenderer
+        extends GeoBlockRenderer<DocumentHolderBlockEntity> {
+    public DocumentHolderBlockEntityRenderer(
+            BlockEntityRendererProvider.Context context) {
+        super(new DocumentHolderGeoModel());
+    }
+
+    @Override
+    public RenderType getRenderType(DocumentHolderBlockEntity animatable,
+            ResourceLocation texture, MultiBufferSource bufferSource,
+            float partialTick) {
+        return RenderType.entityCutoutNoCull(texture);
+    }
+}

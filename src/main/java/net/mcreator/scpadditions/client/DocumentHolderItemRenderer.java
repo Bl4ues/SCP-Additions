03c5dd2d8381020ee88c6a0ada9fa322a9e08b27
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-07 21:03:25 -0300

    Add Document Holder item renderer

---
+++ src/main/java/net/mcreator/scpadditions/client/DocumentHolderItemRenderer.java
@@ -0,0 +1,22 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.mcreator.scpadditions.facility.DocumentHolderBlockItem;
+import software.bernie.geckolib.renderer.GeoItemRenderer;
+
+/** Double-sided item renderer for the Document Holder. */
+public final class DocumentHolderItemRenderer
+        extends GeoItemRenderer<DocumentHolderBlockItem> {
+    public DocumentHolderItemRenderer() {
+        super(new DocumentHolderItemGeoModel());
+    }
+
+    @Override
+    public RenderType getRenderType(DocumentHolderBlockItem animatable,
+            ResourceLocation texture, MultiBufferSource bufferSource,
+            float partialTick) {
+        return RenderType.entityCutoutNoCull(texture);
+    }
+}

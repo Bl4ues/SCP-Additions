467c1fb4e55ac5494da17ee1ba3a19ec24597b7a
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-17 20:10:32 -0300

    Target living-entity push handling for SCP-173

--- src/main/java/net/mcreator/scpadditions/mixin/Scp173PushabilityMixin.java
+++ src/main/java/net/mcreator/scpadditions/mixin/Scp173PushabilityMixin.java
@@ -1,14 +1,14 @@
 package net.mcreator.scpadditions.mixin;
 
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.mcreator.scpadditions.entity.Scp173Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-/** Mapping-safe override that prevents entity collision from shoving SCP-173. */
-@Mixin(Entity.class)
+/** Mapping-safe override that prevents living-entity collision from shoving SCP-173. */
+@Mixin(LivingEntity.class)
 public abstract class Scp173PushabilityMixin {
     @Inject(method = "isPushable", at = @At("HEAD"), cancellable = true)
     private void scpAdditions$keepScp173Anchored(

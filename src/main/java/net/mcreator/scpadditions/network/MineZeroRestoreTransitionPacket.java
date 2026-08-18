a6ba47298c72e5cc351e93b4676fe6d0e770da04
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-18 15:03:21 -0300

    Add MineZero restore transition synchronization

---
+++ src/main/java/net/mcreator/scpadditions/network/MineZeroRestoreTransitionPacket.java
@@ -0,0 +1,44 @@
+package net.mcreator.scpadditions.network;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.network.NetworkEvent;
+import net.mcreator.scpadditions.ScpAdditionsMod;
+
+import java.lang.reflect.Method;
+import java.util.function.Supplier;
+
+/** Synchronizes the pre/post rewind presentation around a MineZero restore. */
+public record MineZeroRestoreTransitionPacket(boolean beginning) {
+    public static void encode(MineZeroRestoreTransitionPacket message,
+            FriendlyByteBuf buffer) {
+        buffer.writeBoolean(message.beginning);
+    }
+
+    public static MineZeroRestoreTransitionPacket decode(FriendlyByteBuf buffer) {
+        return new MineZeroRestoreTransitionPacket(buffer.readBoolean());
+    }
+
+    public static void handle(MineZeroRestoreTransitionPacket message,
+            Supplier<NetworkEvent.Context> supplier) {
+        NetworkEvent.Context context = supplier.get();
+        context.enqueueWork(() -> dispatchClient(message.beginning));
+        context.setPacketHandled(true);
+    }
+
+    private static void dispatchClient(boolean beginning) {
+        if (FMLEnvironment.dist != Dist.CLIENT) return;
+        try {
+            Class<?> target = Class.forName(
+                    "net.mcreator.scpadditions.client.MineZeroClientState");
+            Method method = target.getMethod(beginning
+                    ? "beginRestore" : "finishRestore");
+            method.invoke(null);
+        } catch (ReflectiveOperationException exception) {
+            ScpAdditionsMod.LOGGER.error(
+                    "Could not synchronize MineZero restore transition",
+                    exception);
+        }
+    }
+}

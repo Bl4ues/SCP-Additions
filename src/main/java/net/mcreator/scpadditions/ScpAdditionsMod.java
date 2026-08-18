760941834e3e1630877b9386caf977bed5cdcc59
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-18 13:46:27 -0300

    Bump network protocol for save-state packets

--- src/main/java/net/mcreator/scpadditions/ScpAdditionsMod.java
+++ src/main/java/net/mcreator/scpadditions/ScpAdditionsMod.java
@@ -123,7 +123,7 @@ public class ScpAdditionsMod {
         Scp914SkinManager.initialize();
     }
 
-    private static final String PROTOCOL_VERSION = "18";
+    private static final String PROTOCOL_VERSION = "19";
     public static final SimpleChannel PACKET_HANDLER =
             NetworkRegistry.newSimpleChannel(
                     new ResourceLocation(MODID, MODID),

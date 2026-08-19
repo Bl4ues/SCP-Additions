faa46d4b5ba4c057d5133746378055b64d81f39c
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-18 23:45:48 -0300

    Register player corpse renderer

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntityRenderers.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntityRenderers.java
@@ -5,6 +5,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.client.PlayerCorpseRenderer;
 import net.mcreator.scpadditions.client.Scp106Renderer;
 import net.mcreator.scpadditions.client.Scp131ARenderer;
 import net.mcreator.scpadditions.client.Scp131BRenderer;
@@ -16,5 +17,6 @@ public class ScpAdditionsModEntityRenderers {
 		event.registerEntityRenderer(ScpAdditionsModEntities.SCP_106.get(), Scp106Renderer::new);
 		event.registerEntityRenderer(ScpAdditionsModEntities.SCP_131_A.get(), Scp131ARenderer::new);
 		event.registerEntityRenderer(ScpAdditionsModEntities.SCP_131_B.get(), Scp131BRenderer::new);
+		event.registerEntityRenderer(ScpAdditionsModEntities.PLAYER_CORPSE.get(), PlayerCorpseRenderer::new);
 	}
 }

40d9750b9bf081f4a7343ec522fad14720a84acf
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-21 18:07:25 -0300

    Harden Roomba pass-through collision

--- src/main/java/net/mcreator/scpadditions/mixin/RoombaNoEntityPushMixin.java
+++ src/main/java/net/mcreator/scpadditions/mixin/RoombaNoEntityPushMixin.java
@@ -1,6 +1,9 @@
 package net.mcreator.scpadditions.mixin;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.Level;
 import net.mcreator.scpadditions.entity.RoombaEntity;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -10,7 +13,13 @@ import org.spongepowered.asm.mixin.Mixin;
  * removes the missing outbound half so mobs and players can simply overlap it.
  */
 @Mixin(value = RoombaEntity.class, remap = false)
-public abstract class RoombaNoEntityPushMixin {
+public abstract class RoombaNoEntityPushMixin extends PathfinderMob {
+    protected RoombaNoEntityPushMixin(
+            EntityType<? extends PathfinderMob> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
     protected void doPush(Entity other) {
         // Intentionally empty.
     }

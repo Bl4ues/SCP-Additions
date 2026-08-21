03a5be27441b979da52e087be2910cf1511acf6b
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-21 01:25:51 -0300

    Register SCP-939 renderer

---
+++ src/main/java/net/mcreator/scpadditions/client/Scp939ClientModEvents.java
@@ -0,0 +1,21 @@
+package net.mcreator.scpadditions.client;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.mcreator.scpadditions.ScpAdditionsMod;
+import net.mcreator.scpadditions.init.ScpAdditionsModEntities;
+
+@Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID,
+        bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public final class Scp939ClientModEvents {
+    private Scp939ClientModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ScpAdditionsModEntities.SCP_939.get(),
+                Scp939Renderer::new);
+    }
+}

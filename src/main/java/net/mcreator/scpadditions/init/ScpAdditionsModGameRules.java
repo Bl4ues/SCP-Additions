3f12e64f465c90aa25ea512d440fa4540bf58944
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-21 01:22:08 -0300

    Add SCP-939 spawn gamerule

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModGameRules.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModGameRules.java
@@ -16,5 +16,6 @@ public class ScpAdditionsModGameRules {
 	public static final GameRules.Key<GameRules.BooleanValue> DECONCHECKPOINT = GameRules.register("deconCheckpoint", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
 	public static final GameRules.Key<GameRules.BooleanValue> SCP_173_SPAWN = GameRules.register("173spawn", GameRules.Category.MOBS, GameRules.BooleanValue.create(true));
 	public static final GameRules.Key<GameRules.BooleanValue> SCP_106_SPAWN = GameRules.register("106spawn", GameRules.Category.MOBS, GameRules.BooleanValue.create(true));
+	public static final GameRules.Key<GameRules.BooleanValue> SCP_939_SPAWN = GameRules.register("939spawn", GameRules.Category.MOBS, GameRules.BooleanValue.create(true));
 	public static final GameRules.Key<GameRules.BooleanValue> ROOMBA_SPAWN = GameRules.register("roombaSpawn", GameRules.Category.MOBS, GameRules.BooleanValue.create(true));
 }

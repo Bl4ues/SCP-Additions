f5cf3bfafa1ceb4cf41582aac7b205468115057c
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-21 01:24:24 -0300

    Register SCP-939 entity

--- src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
+++ src/main/java/net/mcreator/scpadditions/init/ScpAdditionsModEntities.java
@@ -17,6 +17,7 @@ import net.mcreator.scpadditions.entity.Scp106Entity;
 import net.mcreator.scpadditions.entity.Scp131AEntity;
 import net.mcreator.scpadditions.entity.Scp131BEntity;
 import net.mcreator.scpadditions.entity.Scp173Entity;
+import net.mcreator.scpadditions.entity.Scp939Entity;
 
 @Mod.EventBusSubscriber(modid = ScpAdditionsMod.MODID,
         bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -46,11 +47,14 @@ public class ScpAdditionsModEntities {
     public static final RegistryObject<EntityType<Scp173Entity>> SCP_173 =
             REGISTRY.register("scp_173", () -> EntityType.Builder.of(
                     Scp173Entity::new, MobCategory.MONSTER)
-                    // Keep the rendered statue broad, but use a player-like
-                    // collision footprint so slight path-node offsets cannot
-                    // wedge it against one-block facility door frames.
                     .sized(0.70F, 1.90F).clientTrackingRange(12)
                     .updateInterval(1).build("scp_173"));
+
+    public static final RegistryObject<EntityType<Scp939Entity>> SCP_939 =
+            REGISTRY.register("scp_939", () -> EntityType.Builder.of(
+                    Scp939Entity::new, MobCategory.MONSTER)
+                    .sized(1.20F, 1.48F).clientTrackingRange(12)
+                    .updateInterval(1).build("scp_939"));
 
     public static final RegistryObject<EntityType<RoombaEntity>> ROOMBA =
             REGISTRY.register("roomba", () -> EntityType.Builder.of(
@@ -72,15 +76,14 @@ public class ScpAdditionsModEntities {
     @SubscribeEvent
     public static void createAttributes(EntityAttributeCreationEvent event) {
         event.put(SCP_106.get(), Scp106Entity.createAttributes().build());
-        event.put(SCP_131_A.get(),
-                AbstractScp131Entity.createAttributes().build());
-        event.put(SCP_131_B.get(),
-                AbstractScp131Entity.createAttributes().build());
+        event.put(SCP_131_A.get(), AbstractScp131Entity.createAttributes().build());
+        event.put(SCP_131_B.get(), AbstractScp131Entity.createAttributes().build());
         event.put(SCP_173.get(), Scp173Entity.createAttributes()
                 .add(Attributes.ARMOR, 80.0D)
                 .add(Attributes.ARMOR_TOUGHNESS, 40.0D)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D)
                 .build());
+        event.put(SCP_939.get(), Scp939Entity.createAttributes().build());
         event.put(ROOMBA.get(), RoombaEntity.createAttributes().build());
         event.put(PLAYER_CORPSE.get(), PlayerCorpseEntity.createAttributes().build());
     }

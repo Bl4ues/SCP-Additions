d59a4b600e56a36f021ee05e9d0e194aed70d097
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-24 13:53:24 -0300

    Update SCP-902 nickname

--- src/main/java/com/bl4ues/scpclassifieddirective/block/Scp902ClosedBlock.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/block/Scp902ClosedBlock.java
@@ -19,7 +19,7 @@ public final class Scp902ClosedBlock extends Scp902BlockBase {
     public void appendHoverText(ItemStack stack, BlockGetter level,
             List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, level, tooltip, flag);
-        tooltip.add(Component.literal("The Final Countdown"));
+        tooltip.add(Component.literal("The Anticipation is Killing Me"));
     }
 
     @Override

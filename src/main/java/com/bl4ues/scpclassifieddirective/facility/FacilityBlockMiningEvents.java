cb4e33bd1573af99e6e507d43e24367340c196bc
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-25 09:44:16 -0300

    Apply facility durability rules to containment units

--- src/main/java/com/bl4ues/scpclassifieddirective/facility/FacilityBlockMiningEvents.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/facility/FacilityBlockMiningEvents.java
@@ -147,6 +147,7 @@ public final class FacilityBlockMiningEvents {
                 addRegistered(blocks, MirroredDoorButtons.BLOCKS);
                 addRegistered(blocks, LeftDoorButtons.BLOCKS);
                 addRegistered(blocks, CoreRoomElevatorModule.BLOCKS);
+                addRegistered(blocks, ObjectContainmentUnitModule.BLOCKS);
                 blocks.add(DocumentHolderModule.block());
                 addGeneratedFacilityBlocks(blocks);
                 addRegisteredBlock(blocks, "tesla_gate_collision");

74802614b1d39d3006fe82cddd05f6be15facf9a
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-26 02:31:18 -0300

    Migrate Tesla Gate states to GeckoLib controller

--- src/main/java/com/bl4ues/scpclassifieddirective/init/ScpClassifiedDirectiveModBlockEntities.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/init/ScpClassifiedDirectiveModBlockEntities.java
@@ -18,6 +18,7 @@ import com.bl4ues.scpclassifieddirective.block.entity.Scp330BlockEntity;
 import com.bl4ues.scpclassifieddirective.block.entity.Scp902BlockEntity;
 import com.bl4ues.scpclassifieddirective.block.entity.Scp1176BlockEntity;
 import com.bl4ues.scpclassifieddirective.block.entity.SystemTerminalBlockEntity;
+import com.bl4ues.scpclassifieddirective.block.entity.TeslaGateBlockEntity;
 import com.bl4ues.scpclassifieddirective.ScpClassifiedDirectiveMod;
 
 public class ScpClassifiedDirectiveModBlockEntities {
@@ -25,6 +26,10 @@ public class ScpClassifiedDirectiveModBlockEntities {
 	public static final RegistryObject<BlockEntityType<Scp330BlockEntity>> SCP_330 = REGISTRY.register("scp_330", () -> BlockEntityType.Builder.of(Scp330BlockEntity::new, ScpClassifiedDirectiveModBlocks.SCP_330.get()).build(null));
 	public static final RegistryObject<BlockEntityType<Scp902BlockEntity>> SCP_902 = REGISTRY.register("scp_902", () -> BlockEntityType.Builder.of(Scp902BlockEntity::new, ScpClassifiedDirectiveModBlocks.SCP_902_CLOSED.get(), ScpClassifiedDirectiveModBlocks.SCP_902_OPEN.get()).build(null));
 	public static final RegistryObject<BlockEntityType<Scp1176BlockEntity>> SCP_1176 = REGISTRY.register("scp_1176", () -> BlockEntityType.Builder.of(Scp1176BlockEntity::new, ScpClassifiedDirectiveModBlocks.SCP_1176.get()).build(null));
+	public static final RegistryObject<BlockEntityType<TeslaGateBlockEntity>> TESLA_GATE = REGISTRY.register("tesla_gate", () -> BlockEntityType.Builder.of(TeslaGateBlockEntity::new,
+			ScpClassifiedDirectiveModBlocks.TESLA_GATE.get(), ScpClassifiedDirectiveModBlocks.TESLA_ACTIVE.get(),
+			ScpClassifiedDirectiveModBlocks.TESLA_ACTIVE_2.get(), ScpClassifiedDirectiveModBlocks.TESLA_ACTIVE_3.get(),
+			ScpClassifiedDirectiveModBlocks.TESLA_ACTIVE_4.get(), ScpClassifiedDirectiveModBlocks.TESLA_RECHARGE.get()).build(null));
 	public static final RegistryObject<BlockEntityType<SystemTerminalBlockEntity>> SCP_079_SYSTEM_CONTROL = REGISTRY.register("scp_079_system_control", () -> BlockEntityType.Builder.of(SystemTerminalBlockEntity::new, ScpClassifiedDirectiveModBlocks.SCP_079_SYSTEM_CONTROL.get()).build(null));
 	public static final RegistryObject<BlockEntityType<?>> SCP_294 = register("scp_294", ScpClassifiedDirectiveModBlocks.SCP_294, Scp294BlockEntity::new);
 	public static final RegistryObject<BlockEntityType<?>> SCP_294_OUT_OF_RANGE = register("scp_294_out_of_range", ScpClassifiedDirectiveModBlocks.SCP_294_OUT_OF_RANGE, Scp294OutOfRangeBlockEntity::new);

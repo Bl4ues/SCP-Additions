fa05e35aa885b6d6175d481ea01415a8a507f296
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-27 10:27:38 -0300

    Let Decontamination BlockEntity own idle scanning

--- src/main/java/com/bl4ues/scpclassifieddirective/block/DeconOpenBlock.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/block/DeconOpenBlock.java
@@ -1,12 +1,6 @@
 package com.bl4ues.scpclassifieddirective.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import com.bl4ues.scpclassifieddirective.procedures.DeconOpenUpdateTickProcedure;
-
+/** Public idle endpoint for the rebuilt GeckoLib Decontamination Checkpoint. */
 public class DeconOpenBlock extends AbstractDecontaminationBlock {
     @Override
     protected boolean isClosedState() {
@@ -16,22 +10,5 @@ public class DeconOpenBlock extends AbstractDecontaminationBlock {
     @Override
     protected boolean raisesOnInitialPlacement() {
         return true;
-    }
-
-    @Override
-    protected void controllerPlaced(BlockState state, Level level,
-            BlockPos pos, BlockState oldState, boolean moving) {
-        level.scheduleTick(pos, this, 5);
-    }
-
-    @Override
-    public void tick(BlockState state, ServerLevel level, BlockPos pos,
-            RandomSource random) {
-        ensureStructure(level, pos, state);
-        DeconOpenUpdateTickProcedure.execute(level,
-                pos.getX(), pos.getY(), pos.getZ());
-        if (level.getBlockState(pos).is(this)) {
-            level.scheduleTick(pos, this, 5);
-        }
     }
 }

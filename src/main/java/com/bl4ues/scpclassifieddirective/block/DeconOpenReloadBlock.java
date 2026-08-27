f5637d08774e6100e7232244290618b534db6fe6
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-27 10:27:56 -0300

    Keep legacy Decontamination reload state compatible

--- src/main/java/com/bl4ues/scpclassifieddirective/block/DeconOpenReloadBlock.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/block/DeconOpenReloadBlock.java
@@ -1,29 +1,9 @@
 package com.bl4ues.scpclassifieddirective.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import com.bl4ues.scpclassifieddirective.procedures.DeconOpenReloadBlockAddedProcedure;
-
+/** Legacy reload ID retained only so old worlds can normalize back to idle. */
 public class DeconOpenReloadBlock extends AbstractDecontaminationBlock {
     @Override
     protected boolean isClosedState() {
         return false;
-    }
-
-    @Override
-    protected void controllerPlaced(BlockState state, Level level,
-            BlockPos pos, BlockState oldState, boolean moving) {
-        level.scheduleTick(pos, this, 40);
-    }
-
-    @Override
-    public void tick(BlockState state, ServerLevel level, BlockPos pos,
-            RandomSource random) {
-        ensureStructure(level, pos, state);
-        DeconOpenReloadBlockAddedProcedure.execute(level,
-                pos.getX(), pos.getY(), pos.getZ());
     }
 }

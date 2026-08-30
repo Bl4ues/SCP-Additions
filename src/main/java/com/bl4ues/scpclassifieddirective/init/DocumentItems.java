64e278c53f3db92cf3154ab2b798dd12d9768404
Deyvid Campos <deyvid04campos@gmail.com>
2026-08-30 13:19:52 -0300

    Place blank document in Items sections

--- src/main/java/com/bl4ues/scpclassifieddirective/init/DocumentItems.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/init/DocumentItems.java
@@ -2,8 +2,6 @@ package com.bl4ues.scpclassifieddirective.init;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -32,14 +30,5 @@ public final class DocumentItems {
     public static void register(RegisterEvent event) {
         event.register(ForgeRegistries.Keys.ITEMS, DOCUMENT_ID,
                 DocumentItem::new);
-    }
-
-    @SubscribeEvent
-    public static void addToCreativeTab(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey().equals(ScpClassifiedDirectiveModTabs.SCP_CLASSIFIED_DIRECTIVE.getKey())) {
-            // Keep the catalog stack uninitialized. Each copy receives its own
-            // Codex ID on first use instead of cloning one tab-wide UUID.
-            event.accept(new ItemStack(getDocument()));
-        }
     }
 }

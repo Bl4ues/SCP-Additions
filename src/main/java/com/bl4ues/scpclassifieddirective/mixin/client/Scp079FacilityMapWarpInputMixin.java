f5b840fe5b64dfbfc47e293e4d20881d301205f4
Deyvid Campos <deyvid04campos@gmail.com>
2026-09-04 22:18:59 -0300

    Correct SCP-079 CRT mouse remapping

--- src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapWarpInputMixin.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapWarpInputMixin.java
@@ -2,6 +2,7 @@ package com.bl4ues.scpclassifieddirective.mixin.client;
 
 import com.bl4ues.scpclassifieddirective.client.scp079.Scp079CrtPostProcessor;
 import com.bl4ues.scpclassifieddirective.client.scp079.Scp079FacilityMapScreen;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -14,37 +15,42 @@ public abstract class Scp079FacilityMapWarpInputMixin {
     private int scpclassifieddirective$renderMouseX(int value) {
         Scp079FacilityMapScreen self = (Scp079FacilityMapScreen) (Object) this;
         return (int) Math.round(Scp079CrtPostProcessor.logicalX(value,
-                scpclassifieddirective$renderMouseYRaw, self.width, self.height));
+                rawMouseY(self), self.width, self.height));
     }
-
-    private int scpclassifieddirective$renderMouseYRaw;
 
     @ModifyVariable(method = "render", at = @At("HEAD"), argsOnly = true,
             ordinal = 1)
     private int scpclassifieddirective$renderMouseY(int value) {
-        scpclassifieddirective$renderMouseYRaw = value;
-        return value;
+        Scp079FacilityMapScreen self = (Scp079FacilityMapScreen) (Object) this;
+        return (int) Math.round(Scp079CrtPostProcessor.logicalY(
+                rawMouseX(self), value, self.width, self.height));
     }
 
     @ModifyVariable(method = {"mouseClicked", "mouseDragged", "mouseReleased", "mouseScrolled"},
             at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private double scpclassifieddirective$mouseX(double value) {
         Scp079FacilityMapScreen self = (Scp079FacilityMapScreen) (Object) this;
-        return Scp079CrtPostProcessor.logicalX(value,
-                scpclassifieddirective$mouseYRaw, self.width, self.height);
+        return Scp079CrtPostProcessor.logicalX(value, rawMouseY(self),
+                self.width, self.height);
     }
-
-    private double scpclassifieddirective$mouseYRaw;
 
     @ModifyVariable(method = {"mouseClicked", "mouseDragged", "mouseReleased", "mouseScrolled"},
             at = @At("HEAD"), argsOnly = true, ordinal = 1)
     private double scpclassifieddirective$mouseY(double value) {
         Scp079FacilityMapScreen self = (Scp079FacilityMapScreen) (Object) this;
-        double result = Scp079CrtPostProcessor.logicalY(
-                scpclassifieddirective$mouseXRaw, value, self.width, self.height);
-        scpclassifieddirective$mouseYRaw = value;
-        return result;
+        return Scp079CrtPostProcessor.logicalY(rawMouseX(self), value,
+                self.width, self.height);
     }
 
-    private double scpclassifieddirective$mouseXRaw;
+    private static double rawMouseX(Scp079FacilityMapScreen screen) {
+        Minecraft minecraft = Minecraft.getInstance();
+        return minecraft.mouseHandler.xpos() * screen.width
+                / Math.max(1.0D, minecraft.getWindow().getScreenWidth());
+    }
+
+    private static double rawMouseY(Scp079FacilityMapScreen screen) {
+        Minecraft minecraft = Minecraft.getInstance();
+        return minecraft.mouseHandler.ypos() * screen.height
+                / Math.max(1.0D, minecraft.getWindow().getScreenHeight());
+    }
 }

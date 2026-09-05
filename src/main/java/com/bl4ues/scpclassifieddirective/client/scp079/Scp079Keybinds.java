067f977b407e3190ae19d427d9badd9ee0835161
Deyvid Campos <deyvid04campos@gmail.com>
2026-09-05 20:46:28 -0300

    Add SCP-079 blackout and lockdown keybinds

--- src/main/java/com/bl4ues/scpclassifieddirective/client/scp079/Scp079Keybinds.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/client/scp079/Scp079Keybinds.java
@@ -11,6 +11,16 @@ public final class Scp079Keybinds {
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_B,
             "key.categories.scp_classified_directive");
+    public static final KeyMapping BLACKOUT = new KeyMapping(
+            "key.scp_classified_directive.scp_079_blackout",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_R,
+            "key.categories.scp_classified_directive");
+    public static final KeyMapping LOCKDOWN = new KeyMapping(
+            "key.scp_classified_directive.scp_079_lockdown",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_F,
+            "key.categories.scp_classified_directive");
 
     private Scp079Keybinds() {
     }

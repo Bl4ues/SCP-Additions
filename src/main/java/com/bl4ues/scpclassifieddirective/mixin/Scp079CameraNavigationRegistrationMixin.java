d286ff0403a7503e04085bf3683300bc3043362e
Deyvid Campos <deyvid04campos@gmail.com>
2026-09-05 20:46:19 -0300

    Register SCP-079 room ability packets

--- src/main/java/com/bl4ues/scpclassifieddirective/mixin/Scp079CameraNavigationRegistrationMixin.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/mixin/Scp079CameraNavigationRegistrationMixin.java
@@ -2,17 +2,19 @@ package com.bl4ues.scpclassifieddirective.mixin;
 
 import com.bl4ues.scpclassifieddirective.network.Scp079CameraNavigationNetwork;
 import com.bl4ues.scpclassifieddirective.network.Scp079PlayableNetwork;
+import com.bl4ues.scpclassifieddirective.network.Scp079RoomAbilityNetwork;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/** Registers the playable SCP-079 camera-topology extension beside its base channel. */
+/** Registers playable SCP-079 extensions beside its base network channel. */
 @Mixin(value = Scp079PlayableNetwork.class, remap = false)
 public abstract class Scp079CameraNavigationRegistrationMixin {
     @Inject(method = "register", at = @At("RETURN"), remap = false)
-    private static void scpclassifieddirective$registerCameraNavigation(
+    private static void scpclassifieddirective$registerExtensions(
             CallbackInfo ci) {
         Scp079CameraNavigationNetwork.register();
+        Scp079RoomAbilityNetwork.register();
     }
 }

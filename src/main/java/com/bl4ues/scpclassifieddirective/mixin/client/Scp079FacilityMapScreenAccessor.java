2cff989deb10006726dd76e80157fe725f5aedf8
Deyvid Campos <deyvid04campos@gmail.com>
2026-09-04 21:22:02 -0300

    Fix SCP-079 map accessor remapping

--- src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapScreenAccessor.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapScreenAccessor.java
@@ -7,9 +7,9 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 /** Read-only state needed by exact custom-drawn SCP-079 button audio hitboxes. */
 @Mixin(Scp079FacilityMapScreen.class)
 public interface Scp079FacilityMapScreenAccessor {
-    @Accessor("leaveConfirmation")
+    @Accessor(value = "leaveConfirmation", remap = false)
     boolean scpclassifieddirective$isLeaveConfirmationOpen();
 
-    @Accessor("floorMenuOpen")
+    @Accessor(value = "floorMenuOpen", remap = false)
     boolean scpclassifieddirective$isFloorMenuOpen();
 }

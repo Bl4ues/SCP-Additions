48247cfc2da8ef864599831c0cc26058db18ced0
Deyvid Campos <deyvid04campos@gmail.com>
2026-09-07 20:33:24 -0300

    Expose SCP-079 map hover target

--- src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapScreenAccessor.java
+++ src/main/java/com/bl4ues/scpclassifieddirective/mixin/client/Scp079FacilityMapScreenAccessor.java
@@ -1,10 +1,11 @@
 package com.bl4ues.scpclassifieddirective.mixin.client;
 
 import com.bl4ues.scpclassifieddirective.client.scp079.Scp079FacilityMapScreen;
+import com.bl4ues.scpclassifieddirective.facility.mapping.FacilityRoomSnapshot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-/** Read-only state needed by exact custom-drawn SCP-079 button audio hitboxes. */
+/** Read-only state needed by custom SCP-079 map overlays and button audio. */
 @Mixin(Scp079FacilityMapScreen.class)
 public interface Scp079FacilityMapScreenAccessor {
     @Accessor(value = "leaveConfirmation", remap = false)
@@ -12,4 +13,7 @@ public interface Scp079FacilityMapScreenAccessor {
 
     @Accessor(value = "floorMenuOpen", remap = false)
     boolean scpclassifieddirective$isFloorMenuOpen();
+
+    @Accessor(value = "hoveredRoom", remap = false)
+    FacilityRoomSnapshot scpclassifieddirective$hoveredRoom();
 }
